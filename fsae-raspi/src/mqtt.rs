99f548e7972d4e8d42a6f8883d9ff43153d8cb37
Alistair Keiller <alistair@keiller.net>
2024-11-12 08:13:02 +0000

    localhost

--- fsae-raspi/src/mqtt.rs
+++ fsae-raspi/src/mqtt.rs
@@ -1,4 +1,4 @@
 pub const MQTT_ID: &str = "raspi";
-pub const MQTT_HOST: &str = "0.0.0.0";
+pub const MQTT_HOST: &str = "localhost";
 pub const MQTT_PORT: u16 = 1883;
 pub const MQTT_TOPIC: &str = "fsae/";
