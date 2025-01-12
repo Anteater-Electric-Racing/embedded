c03347296a7c0b603f221142f21f9e6c44cf011e
Alistair Keiller <alistair@keiller.net>
2025-01-12 15:19:39 -0800

    format main to 4

--- fsae-vehicle-fw/src/main.cpp
+++ fsae-vehicle-fw/src/main.cpp
@@ -7,10 +7,10 @@
 // change this to use freertos
 
 void setup() {
-  // put your setup code here, to run once:
-  Peripherals_init();
+    // put your setup code here, to run once:
+    Peripherals_init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
