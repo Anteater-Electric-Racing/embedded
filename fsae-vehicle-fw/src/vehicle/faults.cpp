7fe50b02c45e5cbbf59f173a7181397c11b13561
billofright <bill.yuan27@gmail.com>
2025-01-21 11:23:00 -0800

    fault module clean

--- fsae-vehicle-fw/src/vehicle/faults.cpp
+++ fsae-vehicle-fw/src/vehicle/faults.cpp
@@ -1,9 +1,16 @@
 // Anteater Electric Racing, 2025
 
+#define FAULT_OVER_CURRENT_MASK 0x1
+#define FAULT_UNDER_VOLTAGE_MASK 0x1 << 1
+#define FAULT_OVER_TEMP_MASK 0x1 << 2
+#define FAULT_APPS_MASK 0x1 << 3
+#define FAULT_BSE_MASK 0x1 << 4
+#define FAULT_APPS_BRAKE_PLAUSIBILITY_MASK 0x1 << 5
+
 #include "vehicle/faults.h"
 #include "vehicle/motor.h"
 
-uint32_t faultBitMap;
+static uint32_t faultBitMap;
 
 void Faults_Init(){
     faultBitMap = 0;
@@ -17,32 +24,32 @@ void Faults_SetFault(FaultType fault){
         }
         case FAULT_OVER_CURRENT:
         {
-            faultBitMap |= FAULT_OVER_CURRENT_OFFSET;
+            faultBitMap |= FAULT_OVER_CURRENT_MASK;
             break;
         }
         case FAULT_UNDER_VOLTAGE:
         {
-            faultBitMap |= FAULT_UNDER_VOLTAGE_OFFSET;
+            faultBitMap |= FAULT_UNDER_VOLTAGE_MASK;
             break;
         }
         case FAULT_OVER_TEMP:
         {
-            faultBitMap |= FAULT_OVER_TEMP_OFFSET;
+            faultBitMap |= FAULT_OVER_TEMP_MASK;
             break;
         }
         case FAULT_APPS:
         {
-            faultBitMap |= FAULT_APPS_OFFSET;
+            faultBitMap |= FAULT_APPS_MASK;
             break;
         }
         case FAULT_BSE:
         {
-            faultBitMap |= FAULT_BSE_OFFSET;
+            faultBitMap |= FAULT_BSE_MASK;
             break;
         }
         case FAULT_APPS_BRAKE_PLAUSIBILITY:
         {
-            faultBitMap |= FAULT_APPS_BRAKE_PLAUSIBILITY_OFFSET;
+            faultBitMap |= FAULT_APPS_BRAKE_PLAUSIBILITY_MASK;
             break;
         }
         default:
@@ -50,11 +57,6 @@ void Faults_SetFault(FaultType fault){
             break;
         }
     }
-}
-
-// for telemetry
-uint32_t* Faults_GetFaults() {
-    return &faultBitMap;
 }
 
 void Faults_ClearFault(FaultType fault){
@@ -65,32 +67,32 @@ void Faults_ClearFault(FaultType fault){
         }
         case FAULT_OVER_CURRENT:
         {
-            faultBitMap &= ~FAULT_OVER_CURRENT_OFFSET;
+            faultBitMap &= ~FAULT_OVER_CURRENT_MASK;
             break;
         }
         case FAULT_UNDER_VOLTAGE:
         {
-            faultBitMap &= ~FAULT_UNDER_VOLTAGE_OFFSET;
+            faultBitMap &= ~FAULT_UNDER_VOLTAGE_MASK;
             break;
         }
         case FAULT_OVER_TEMP:
         {
-            faultBitMap &= ~FAULT_OVER_TEMP_OFFSET;
+            faultBitMap &= ~FAULT_OVER_TEMP_MASK;
             break;
         }
         case FAULT_APPS:
         {
-            faultBitMap &= ~FAULT_APPS_OFFSET;
+            faultBitMap &= ~FAULT_APPS_MASK;
             break;
         }
         case FAULT_BSE:
         {
-            faultBitMap &= ~FAULT_BSE_OFFSET;
+            faultBitMap &= ~FAULT_BSE_MASK;
             break;
         }
         case FAULT_APPS_BRAKE_PLAUSIBILITY:
         {
-            faultBitMap &= ~FAULT_APPS_BRAKE_PLAUSIBILITY_OFFSET;
+            faultBitMap &= ~FAULT_APPS_BRAKE_PLAUSIBILITY_MASK;
             break;
         }
         default:
@@ -100,94 +102,33 @@ void Faults_ClearFault(FaultType fault){
     }
 }
 
-// need struct of car info
-// fault logging?
-void Faults_CheckFaults() {
-    if (checkOverCurrent())
-        Faults_SetFault(FAULT_OVER_CURRENT);
-    if (checkUnderVoltage())
-        Faults_SetFault(FAULT_UNDER_VOLTAGE);
-    if (checkOverTemp())
-        Faults_SetFault(FAULT_OVER_TEMP);
-    if (checkAPPS())
-        Faults_SetFault(FAULT_APPS);
-    if (checkBSE())
-        Faults_SetFault(FAULT_BSE);
-    if (checkABP())
-        Faults_SetFault(FAULT_APPS_BRAKE_PLAUSIBILITY);
+// currently having all faults being handled the same but leaving room for future customization
+void Faults_HandleFaults(){
+    if(faultBitMap & FAULT_OVER_CURRENT_MASK){
+        Motor_SetFaultState();
+    }
+    if(faultBitMap & FAULT_UNDER_VOLTAGE_MASK){
+        Motor_SetFaultState();
+    }
+    if(faultBitMap & FAULT_OVER_TEMP_MASK){
+        Motor_SetFaultState();
+    }
+    if(faultBitMap & FAULT_APPS_MASK){
+        Motor_SetFaultState();
+    }
+    if(faultBitMap & FAULT_BSE_MASK){
+        Motor_SetFaultState();
+    }
+    if(faultBitMap & FAULT_APPS_BRAKE_PLAUSIBILITY_MASK){
+        Motor_SetFaultState();
+    }
 }
 
-
-bool checkOverCurrent() {
-    // battery shutdown
-    return false;
+// for telemetry
+uint32_t* Faults_GetFaults() {
+    return &faultBitMap;
 }
 
-bool checkUnderVoltage() {
-    // battery shutdown
-    return false;
+bool Faults_CheckAllClear(){
+    return faultBitMap == 0;
 }
-
-bool checkOverTemp() {
-    // battery shutdown
-    return false;
-}
-
-bool checkAPPS() {
-    // APPS check for 10% difference in throttle inputs
-    // Motor_SetFaultState();
-    return false;
-}
-
-bool checkBSE() {
-    // BSE check for signal in range of 0.5-4.5 V
-    // Motor_SetFaultState();
-    return false;
-}
-
-bool checkABP() {
-    // ABP check for brakes engaged + APPS more than 25% throttle
-    // Motor_SetFaultState();
-    return false;
-}
-
-// void Faults_HandleFaults() {
-//     if (faultMap.overCurrent) {
-//         // battery on
-//         Faults_ClearFault(FAULT_OVER_CURRENT);
-//     }
-//     if (faultMap.underVoltage) {
-//         // battery on
-//         Faults_ClearFault(FAULT_UNDER_VOLTAGE);
-//     }
-//     if (faultMap.overTemp) {
-//         // battery on
-//         Faults_ClearFault(FAULT_OVER_TEMP);
-//     }
-//     if (faultMap.APPS_fault) {
-//         // clear fault and motor on when throttle is within 10% difference
-//         Faults_ClearFault(FAULT_APPS);
-//     }
-//     if (faultMap.BSE_fault) {
-//         // clear fault and motor on when brake signal is within range 0.5-4.5 V
-//         Faults_ClearFault(FAULT_BSE);
-//     }
-//     if (faultMap.APPS_brake_plausibility) {
-//         // clear fault and motor on when throttle is less than 5% travel
-//         Faults_ClearFault(FAULT_APPS_BRAKE_PLAUSIBILITY);
-//     }
-
-//     // final check before reset
-//     if (Faults_CheckAllClear()) {
-//         // check if motor/battery faulted
-//             // set motor state to driving
-//             // reset battery
-//     }
-// }
-
-// bool Faults_CheckAllClear() {
-//     return !faultMap.overCurrent && !faultMap.underVoltage && !faultMap.overTemp && !faultMap.APPS_fault && !faultMap.BSE_fault && !faultMap.APPS_brake_plausibility;
-// }
-
-
-// faults.sendallclear() - this would be called whenever a fault is cleared so that if all faults are clear, the system can be reset
--- fsae-vehicle-fw/src/vehicle/faults.h
+++ fsae-vehicle-fw/src/vehicle/faults.h
@@ -3,13 +3,6 @@
 #ifndef FAULTS_H
 #define FAULTS_H
 
-#define FAULT_OVER_CURRENT_OFFSET 0x1
-#define FAULT_UNDER_VOLTAGE_OFFSET 0x1 << 1
-#define FAULT_OVER_TEMP_OFFSET 0x1 << 2
-#define FAULT_APPS_OFFSET 0x1 << 3
-#define FAULT_BSE_OFFSET 0x1 << 4
-#define FAULT_APPS_BRAKE_PLAUSIBILITY_OFFSET 0x1 << 5
-
 #include <stdint.h>
 
 typedef enum {
@@ -22,21 +15,13 @@ typedef enum {
     FAULT_APPS_BRAKE_PLAUSIBILITY
 } FaultType;
 
+
 void Faults_Init();
+
 void Faults_SetFault(FaultType fault);
 uint32_t* Faults_GetFaults();
 void Faults_ClearFault(FaultType fault);
-void Faults_CheckFaults();
 void Faults_HandleFaults();
 bool Faults_CheckAllClear();
-
-bool checkOverCurrent();
-bool checkUnderVoltage();
-bool checkOverTemp();
-bool checkAPPS();
-bool checkBSE();
-bool checkABP();
-
-
 
 #endif
