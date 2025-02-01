537b44f5d8df9e4de28c7144adba43e0861d7c9c
Jaslavie <jasmineqw45@gmail.com>
2025-02-01 10:34:47 -0800

    added regen conditions

--- fsae-vehicle-fw/src/utils/regen.cpp
+++ fsae-vehicle-fw/src/utils/regen.cpp
@@ -17,28 +17,41 @@ float Motor_CalculateRegenBraking(float brakePosition, float currentSpeed, float
     //TODO: check over the formula
     float wheelRadius = 0.3302; //TODO: get actual wheel radius
 
-    if (currentSpeed > MAX_REGEN_SPEED) {
-        // apply braking and friction
+    if (Motor_EnableRegenBraking(currentSpeed, brakePosition)) {
+        // calculate energy loss
+        float energyLoss = 1/2*mass*(pow(currentSpeed, 2) - pow(initialSpeed, 2));
+
+        // calculate available regen energy
+        float availableRegenEnergy = energyLoss * 0.85; //TODO: change constant to actual efficiency of braking system
+
+        // calculate regen torque
+        float regenTorque = availableRegenEnergy / wheelRadius;
+
+        // check that torque does not exceed threshold
+        if (regenTorque > MAX_REGEN_TORQUE) {
+            return MAX_REGEN_TORQUE;
+        };
+
+        return regenTorque;
+    }
+
+    return 0.0F;
+}
+
+/**
+ * Conditions to enable regen braking:
+ * - speed is within regen range (under MAX_REGEN_SPEED)
+ * - brake force is above a threshold (if > threshold then use regen)
+ * - torque can be handled by motor (if < MAX_REGEN_TORQUE)
+ */
+bool Motor_EnableRegenBraking(float currentSpeed, float brakePosition, float regenTorque) {
+    // do not enable regen
+    if (currentSpeed > MAX_REGEN_SPEED // speed is too high
+        || brakePosition < REGEN_BRAKE_THRESHOLD // brake is not powerful enough
+        || regenTorque > MAX_REGEN_TORQUE) { // torque is too high
+
+        return false;
     };
 
-    if (brakePosition < REGEN_BRAKE_THRESHOLD) {
-        // do not use regn if brake is not powerful enough
-        return 0.0F;
-    };
-
-    // calculate energy loss
-    float energyLoss = 1/2*mass*(pow(currentSpeed, 2) - pow(initialSpeed, 2));
-
-    // calculate available regen energy
-    float availableRegenEnergy = energyLoss * 0.85; //TODO: change constant to actual efficiency of braking system
-
-    // calculate regen torque
-    float regenTorque = availableRegenEnergy / wheelRadius;
-
-    // check that torque does not exceed threshold
-    if (regenTorque > MAX_REGEN_TORQUE) {
-        return MAX_REGEN_TORQUE;
-    };
-
-    return regenTorque;
+    return true;
 }
