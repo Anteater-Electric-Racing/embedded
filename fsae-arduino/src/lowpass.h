63f2b1690682eda05caf6099a0d181cf4771006d
Username123-coder <73372807+Username123-coder@users.noreply.github.com>
2025-02-06 14:33:21 -0800

    Low pass edits

--- fsae-arduino/src/lowpass.h
+++ fsae-arduino/src/lowpass.h
@@ -1,28 +1,22 @@
 #include <math.h>
 
-#define Alpha(cutoff, freq) {\ 
+#define ALPHA(cutoff, freq) {\ 
             float te = 1.0 / freq ;\
             float tau = 1.0 / (2*M_PI*cutoff) ;\
             (1.0 / (1.0 + tau/te)) ;\
            } 
 
-#define LowPass(value, a, s, init) {\ 
-            if (init)\
-                result = a*value + (1.0-a)*s ;\
-            else {\
-                result = value ;\
-                (init) = true ;\
-            }\
-            (result) ;\
+#define LOW_PASS_FILTER(value, old, alpha) {\ 
+            result = alpha*value + (1.0-alpha)*old ;\
            } 
 
-#define Filter(value, timestamp, dcutoff, mincutoff, init1, init2, last, freq, s, beta_) {\
+#define FILTER(value, timestamp, dcutoff, mincutoff, last, freq, s, beta_) {\
             if (lasttime!=UndefinedTime && timestamp!=UndefinedTime)\
                 freq = 1.0 / (timestamp-lasttime) ;\
             lasttime = timestamp ;\
-            float dvalue = init1 ?  0.0 : (value - last) * freq ;\ 
-            float edvalue = LowPass(dvalue, Alpha(dcutoff, freq), s, init2) ;\
+            float dvalue = (value - last) * freq ;\ 
+            float edvalue = LOW_PASS_FILTER(dvalue, s, ALPHA(dcutoff, freq)) ;\
             float cutoff = mincutoff + beta_*fabs(edvalue) ;\
-            float arr[2] = {LowPass(value, alpha(cutoff), last, init1), edvalue} ;\
+            float arr[2] = {LOW_PASS_FILTER(value, last, ALPHA(cutoff)), edvalue} ;\
             (arr) ;\
             }
