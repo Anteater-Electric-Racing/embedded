4a41f8ebf0d9369a81e6e78b900560a5a9cf4eda
Alistair Keiller <alistair@keiller.net>
2025-02-10 23:10:57 -0800

    format

--- fsae-dashboard/src/mqtt.rs
+++ fsae-dashboard/src/mqtt.rs
@@ -15,7 +15,7 @@ pub enum Event {
 
 pub fn mqtt() -> impl Stream<Item = Event> {
     stream::channel(100, |mut output| async move {
-        let mut state: Option<Connection>= None;
+        let mut state: Option<Connection> = None;
 
         loop {
             match &mut state {
@@ -23,28 +23,23 @@ pub fn mqtt() -> impl Stream<Item = Event> {
                     let mut mqttoptions = MqttOptions::new("dashboard", "127.0.0.1", 1883);
                     mqttoptions.set_keep_alive(Duration::from_secs(5));
                     let (_client, eventloop) = AsyncClient::new(mqttoptions, 10);
-                    if let Err(e) = _client
-                        .subscribe("data", QoS::AtMostOnce)
-                        .await
-                    {
+                    if let Err(e) = _client.subscribe("data", QoS::AtMostOnce).await {
                         eprintln!("Failed to subscribe to data. Error: {}", e);
                         continue;
                     };
                     state = Some(Connection { _client, eventloop });
                 }
-                Some(connection) => {
-                    match connection.eventloop.poll().await {
-                        Ok(notification) => {
-                            if let Err(e) = output.send(Event::Message(notification)).await {
-                                eprintln!("Failed to send message: {}", e);
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to poll eventloop: {}", e);
-                            state = None;
+                Some(connection) => match connection.eventloop.poll().await {
+                    Ok(notification) => {
+                        if let Err(e) = output.send(Event::Message(notification)).await {
+                            eprintln!("Failed to send message: {}", e);
                         }
                     }
-                }
+                    Err(e) => {
+                        eprintln!("Failed to poll eventloop: {}", e);
+                        state = None;
+                    }
+                },
             }
         }
     })
