8dcaef471aa3b3d8636b6e109bf50125059dcc16
Natalie Perrochon <nperroch@uci.edu>
2025-02-19 13:12:20 -0800

    Added second ADC and read from 16 pins

--- fsae-vehicle-fw/src/callbacks.cpp
+++ fsae-vehicle-fw/src/callbacks.cpp
@@ -6,19 +6,36 @@
 
 #define LOGIC_LEVEL_V 3.3
 #define ADC_RESOLUTION 10
-#define ADC1_MAX_VALUE (1 << ADC_RESOLUTION) - 1
+#define adc0_MAX_VALUE (1 << ADC_RESOLUTION) - 1
 
 void StartADCScanCallback() {
     noInterrupts();
-    if (adcIndex != 0){ // means it was not reset at the end of the callback cycle: means callbacks did not complete
-        _reboot_Teensyduino_();
+    if (adc0Index != 0 || adc1Index != 0){ // means it was not reset at the end of the callback cycle: means callbacks did not complete
+       Serial.println(adc0Index);
+       Serial.println(adc1Index);
+        // _reboot_Teensyduino_();
     }
-    adcIndex = 0;
-    uint16_t startPin = adcPins[0];
+    adc0Index = 0;
+    adc1Index = 0;
+    uint16_t startPinADC0 = adc0Pins[0];
+    uint16_t startPinADC1 = adc1Pins[0];
     uint32_t currentTime = micros();
-    adc->adc1->enableInterrupts(ADCConversionCompleteCallback);
-    adc->adc1->startSingleRead(startPin); // in callbacks.h
+    adc->adc0->enableInterrupts(ADCConversionCompleteCallback);
+    adc->startSynchronizedSingleRead(startPinADC0, startPinADC1); // in callbacks.h
     interrupts();
+    Serial.print("ADC 0 reading is ");
+    for (int i = 0; i < SENSOR_PIN_AMT_ADC0; ++i){
+        Serial.print(adc0Reads[i]);
+        Serial.print(", ");
+    }
+    Serial.println();
+
+    Serial.print("ADC 1 reading is ");
+    for (int i = 0; i < SENSOR_PIN_AMT_ADC1; ++i){
+        Serial.print(adc1Reads[i]);
+        Serial.print(", ");
+    }
+    Serial.println();
 }
 
 void ADCConversionCompleteCallback () {
@@ -26,19 +43,64 @@ void ADCConversionCompleteCallback () {
     // store pin reads
     // go to next pin
     noInterrupts();
-    uint16_t sensorRead = adc->adc1->readSingle();
-    adcReads[adcIndex] = sensorRead; //*LOGIC_LEVEL_V/ADC1_MAX_VALUE; // Get value within 0V to 3.3V range
+
     // TODO clear ADC data field if that's possible because otherwise there might be a bug with it reading two values close together too quickly?
 
-    // increment to next sensor pin & start read
-    ++adcIndex;
-    if(adcIndex >= SENSOR_PIN_AMT){ // Do here so we don't start a read for an invalid pin
-        adcIndex = 0;
-    } else {
-        uint16_t currentPin = adcPins[adcIndex];
-        adc->adc1->enableInterrupts(ADCConversionCompleteCallback, adcIndex); // Priority gets lower as it's further in the index array
-        adc->adc1->startSingleRead(currentPin); // in callbacks.h
+
+    // if both are less than the range, read both
+    //  if next index is in both ranges, start sync read
+    //  if next index in 0 range, start read
+    //  if next index in 1 range, start read
+    // if 0 is less than the range and 1 is out of range, read 0 and start 0
+    // if 1 is less than the range and 0 is out of range, read 1 and start 1
+    // if both out of range, move both to 0 and return
+
+    if(adc0Index <= SENSOR_PIN_AMT_ADC0 && adc1Index <= SENSOR_PIN_AMT_ADC1){
+        ADC::Sync_result result = adc->readSynchronizedSingle();
+        uint16_t sensorReadADC0 = result.result_adc0;
+        uint16_t sensorReadADC1 = result.result_adc1;
+
+        adc0Reads[adc0Index] = sensorReadADC0; //*LOGIC_LEVEL_V/adc0_MAX_VALUE; // Get value within 0V to 3.3V range
+        adc1Reads[adc1Index] = sensorReadADC1;
+
+        ++adc0Index;
+        ++adc1Index;
+        if (adc0Index < SENSOR_PIN_AMT_ADC0 && adc1Index < SENSOR_PIN_AMT_ADC1){
+            adc->startSynchronizedSingleRead(adc0Pins[adc0Index], adc1Pins[adc1Index]);
+        }
+    } else if (adc0Index <= SENSOR_PIN_AMT_ADC0){
+        uint16_t sensorReadADC0 = adc->adc0->readSingle();
+        adc0Reads[adc0Index] = sensorReadADC0;
+        ++adc0Index;
+    } else if (adc1Index <= SENSOR_PIN_AMT_ADC1){
+        uint16_t sensorReadADC1 = adc->adc1->readSingle();
+        adc1Reads[adc1Index] = sensorReadADC1;
+        ++adc1Index;
     }
+
+    if(adc0Index >= SENSOR_PIN_AMT_ADC0 && adc1Index >= SENSOR_PIN_AMT_ADC1){ // Do here so we don't start a read for an invalid pin
+        adc0Index = 0;
+        adc1Index = 0;
+    } else if(adc0Index < SENSOR_PIN_AMT_ADC0){ // Do here so we don't start a read for an invalid pin
+        adc->adc0->enableInterrupts(ADCConversionCompleteCallback, adc0Index); // Priority gets lower as it's further in the index array
+        adc->adc0->startSingleRead(adc0Pins[adc0Index]); // in callbacks.h
+    } else if(adc1Index < SENSOR_PIN_AMT_ADC1){ // Do here so we don't start a read for an invalid pin
+        adc->adc1->enableInterrupts(ADCConversionCompleteCallback, adc1Index); // Priority gets lower as it's further in the index array
+        adc->adc1->startSingleRead(adc1Pins[adc1Index]); // in callbacks.h
+    }
+
     interrupts();
+
+
+    // increment to next sensor pin & start read
+    // ++adc0Index;
+    // if(adc0Index >= SENSOR_PIN_AMT_ADC0){ // Do here so we don't start a read for an invalid pin
+    //     adc0Index = 0;
+    // } else {
+    //     uint16_t currentPin = adc0Pins[adc0Index];
+    //     adc->adc0->enableInterrupts(ADCConversionCompleteCallback, adc0Index); // Priority gets lower as it's further in the index array
+    //     adc->adc0->startSingleRead(currentPin); // in callbacks.h
+    // }
+    // interrupts();
 }
 
--- fsae-vehicle-fw/src/peripherals/adc.h
+++ fsae-vehicle-fw/src/peripherals/adc.h
@@ -2,8 +2,9 @@
 
 #include <cstdint>
 #include <ADC.h>
-#define SENSOR_PIN_AMT 8
-enum SensorIndexes {
+#define SENSOR_PIN_AMT_ADC0 8
+#define SENSOR_PIN_AMT_ADC1 8
+enum SensorIndexesADC0 {
     APPS_1_INDEX,
     APPS_2_INDEX,
     BSE_1_INDEX,
@@ -14,10 +15,27 @@ enum SensorIndexes {
     SUSP_TRAV_LINPOT4
 };
 
+enum SensorIndexesADC1 {
+    APPS_1_INDEX2,
+    APPS_2_INDEX2,
+    BSE_1_INDEX2,
+    BSE_2_INDEX2,
+    SUSP_TRAV_LINPOT12,
+    SUSP_TRAV_LINPOT22,
+    SUSP_TRAV_LINPOT32,
+    SUSP_TRAV_LINPOT42
+};
+
 // note: change inputs for potentiometers to A0 (14) and A1 (15)
-static uint16_t adcPins[SENSOR_PIN_AMT] = {A0, A1, A2, A3, A4, A5, A6, A7}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
-static volatile uint16_t adcIndex = 0;
-static volatile uint16_t adcReads[SENSOR_PIN_AMT];
+static uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {A0, A1, A2, A3, A4, A5, A6, A7}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
+static volatile uint16_t adc0Index = 0;
+static volatile uint16_t adc0Reads[SENSOR_PIN_AMT_ADC0];
+
+static uint16_t adc1Pins[SENSOR_PIN_AMT_ADC1] = {A7, A6, A5, A4, A3, A2, A1, A0}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
+static volatile uint16_t adc1Index = 0;
+static volatile uint16_t adc1Reads[SENSOR_PIN_AMT_ADC1];
+
+
 
 static ADC *adc = new ADC();
 
