ee59d3871551fe6ffe98f56da79c36acaa3b8134
WilsonNguyen <wilsonnguyen0928@gmail.com>
2025-04-08 12:07:11 -0700

    .pio\libdeps\teensy41\WDT_T4/Watchdog_t4.tpp:152:20: warning: there are no arguments to 'constrain' that depend on a template parameter, so a declaration of 'constrain' must be available [-fpermissive]   152 |   config.trigger = constrain(config.trigger, 0.0f, 127.5); /* callback trigger before timeout */       |                    ^~~~~~~~~ .pio\libdeps\teensy41\WDT_T4/Watchdog_t4.tpp:152:20: warning: there are no arguments to 'constrain' that depend on a template parameter, so a declaration of 'constrain' must be available [-fpermissive]   152 |   config.trigger = constrain(config.trigger, 0.0f, 127.5); /* callback trigger before timeout */       |                    ^~~~~~~~~# On branch wn/watchdog

--- fsae-vehicle-fw/src/main.cpp
+++ fsae-vehicle-fw/src/main.cpp
@@ -3,53 +3,40 @@
 #define THREAD_MAIN_STACK_SIZE 128
 #define THREAD_MAIN_PRIORITY 1
 
-#define WDT_CHECK_INTERVAL_MS 500
-#define WDT_TIMEOUT_MS 1000
 
 #include <Arduino.h>
 #include <arduino_freertos.h>
 
 #include "peripherals/peripherals.h"
 #include "peripherals/watchdog.h"
-#include "peripherals/watchdog.h"
+
 
 void threadMain( void *pvParameters );
-bool checkSensors();
-
-bool checkSensors(){
-  unsigned long currentTime = millis();
-
-  const unsigned long IMU_TIMEOUT = 300;
-  const unsigned long GPS_TIMEOUT = 1000;
-
-  if (currentTime - sensorLastUpdate.imu > IMU_TIMEOUT){
-    Serial.println("IMU timeout");
-    return false;
-  }
-  if (currentTime - sensorLastUpdate.gps > GPS_TIMEOUT){
-    Serial.println("GPS timeout");
-    return false;
-  }
-
-  // If all sensors are working:
-  return true;
-}
 
 void setup() {
+  
+  
+  
   xTaskCreate(threadMain, "threadMain", THREAD_MAIN_STACK_SIZE, NULL, THREAD_MAIN_PRIORITY, NULL);
+  vTaskStartScheduler();
 }
 
 void threadMain( void *pvParameters ) {
-  Peripherals_init();
-  Watchdog_Init();
+  Serial.begin(9600);
+  // Peripherals_init();
+  // Watchdog_Init();
+
+  //delay(500);
+  // while (!Serial && millis() < 4000); // test
+  Serial.println("System starting..."); // Testing
 
   while (true) {
     // Tells the computer to wait
-    vTaskDelay(pdMS_TO_TICKS(WDT_CHECK_INTERVAL_MS));
+    vTaskDelay(1000);
 
     // Only pet the watchdog if all sensors are working
     if (checkSensors()) {
-        Watchdog_Pet();
+        // Watchdog_Pet();
     }
   }
 }
@@ -57,6 +44,7 @@ void threadMain( void *pvParameters ) {
 void loop() {}
 
 // Update sensor last updates
+/*
 void updateBrakeSensor() {
     sensorLastUpdate.brake = millis();
 }
@@ -76,3 +64,4 @@ void updateImuSensor() {
 void updateGpsSensor() {
     sensorLastUpdate.gps = millis();
 }
+*/
--- fsae-vehicle-fw/src/peripherals/watchdog.cpp
+++ fsae-vehicle-fw/src/peripherals/watchdog.cpp
@@ -2,29 +2,50 @@
 
 #include "watchdog.h"
 #include <Arduino.h>
-// #include <imxrt.h> Uncomment if Teensy provides correct header
+#include <imxrt.h> // Uncomment if Teensy provides correct header
+WDT_T4<WDT1> wdt; // Global WDT instance
 
-void initWatchdog(){
-    // Clear Wide Mode Register 
-    WDOG1_WMCR = 0;
+bool checkSensors(){
+  Serial.println("Testing watchdog without sensors");
+  return true;
+
+  /*unsigned long currentTime = millis();
+
+  const unsigned long IMU_TIMEOUT = 300;
+  const unsigned long GPS_TIMEOUT = 1000;
+
+  if (currentTime - sensorLastUpdate.imu > IMU_TIMEOUT){
+    Serial.println("IMU timeout");
+    return false;
+  }
+  if (currentTime - sensorLastUpdate.gps > GPS_TIMEOUT){
+    Serial.println("GPS timeout");
+    return false;
+  }
+
+  // If all sensors are working:
+  return true; */
+}
+
+void Watchdog_Init(){
+  Serial.println("Initializing watchdog...");
   
-    // Set timeout period (1 sec)
-    WDOG1_WCR = WDOG_WCR_WT(0x3FF);
-  
-    WDOG1_WCR |= WDOG_WCR_WDE;  // Enable WDT
-    WDOG1_WCR |= WDOG_WCR_SRE;  // System Reset Enable
-    WDOG1_WCR &= ~WDOG_WCR_WDA; // Disable alt reset signal WDOG_B
-  
-    // Start timer
-    Watchdog_Pet();
+  /*WDT_timings_t config;
+  config.timeout = WDT_TIMEOUT_SEC;
+  config.window = WDT_WINDOW_SEC; // must feed after 0.5s, before 1s
+  config.trigger = WDT_TRIGGER_SEC; // warning trigger
+  config.callback = myCallBack; // Full system reset (change back to nullptr) 
+
+  wdt.begin(config);*/
+
+  Serial.println("Watchdog initialized!");
 }
 
 void Watchdog_Pet(){
-  // First service key, pause
-  __asm__ volatile ("nop");
-  WDOG1_WSR = 0x5555;
+  wdt.feed();
+  Serial.println("Watchdog has been pet.");
+}
 
-  // Second, pause
-  __asm__ volatile ("nop");
-  WDOG1_WSR = 0xAAAA;
+void myCallBack(){
+  Serial.println("Feed wdt");
 }
