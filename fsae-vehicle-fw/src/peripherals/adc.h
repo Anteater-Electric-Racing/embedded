ff86aacddbac5e670c518d74aec72be3b31cdba6
Natalie Perrochon <nperroch@uci.edu>
2025-04-08 13:48:02 -0700

    Updated ADC0 pins based on diagram in github
    Updated pins as they correspond to the sensor pins in the github wiring diagram

--- fsae-vehicle-fw/src/peripherals/adc.h
+++ fsae-vehicle-fw/src/peripherals/adc.h
@@ -4,8 +4,8 @@
 #define SENSOR_PIN_AMT_ADC0 8
 #define SENSOR_PIN_AMT_ADC1 8
 
-// note: change inputs for potentiometers to A0 (14) and A1 (15)
-static uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {A0, A1, A2, A3, A4, A5, A6, A7}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
+// Last updated ADC0 Pins on 4/8/25 based on pinouts in https://github.com/Anteater-Electric-Racing/Central-Computer-Module
+static uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {19, 18, 17, 16, 23, 22, 21, 20};
 static volatile uint16_t adc0Index = 0;
 static volatile uint16_t adc0Reads[SENSOR_PIN_AMT_ADC0];
 
