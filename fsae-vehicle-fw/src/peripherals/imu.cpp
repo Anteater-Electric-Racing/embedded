b9ddd2e00c8da8b4b927c4844a3ba7f0c810ee32
Rishi <rtheegal@uci.edu>
2025-04-08 21:53:54 -0700

    Added i2c interface for imu

--- fsae-vehicle-fw/src/peripherals/imu.cpp
+++ fsae-vehicle-fw/src/peripherals/imu.cpp
@@ -6,21 +6,55 @@
 #define THREAD_IMU_STACK_SIZE 128
 #define THREAD_IMU_PRIORITY 1
 
+static struct IMUData imu_dat;
+
 // Telemetry object is const pointer bc of mutual exclusion,
 // so we just update local struct and telemetry pulls from it
 
 void IMU_Read(void *pvParameters) {
+
+    Wire.beginTransmission(IMU_ID);
+    Wire.write(IMU_REG);
+    Wire.endTransmission(false);
+    Wire.requestFrom(IMU_ID, num_Reg * 2, true);
+
     while(true){
         // TODO:
         // Check if enough bytes available
-        // Yes Case:
-            // Read bytes
-            // Update local struct (defined in imu.h)
-        // We don't check for no case
+        if (Wire.available() < num_Reg * 2) {
+
+            for (int i = 0; i < num_Reg; i++) {
+                int16_t data = Wire.read() << 8 | Wire.read();
+                data = constrain(map(data, -1700, 1700, 0, 127));
+
+                switch (i) {
+                    case 0: imu_dat.accel_x = data; break;
+                    case 1: imu_dat.accel_y = data; break;
+                    case 2: imu_dat.accel_z = data; break;
+                    case 3: imu_dat.temp = data; break;
+                    case 4: imu_dat.gyro_x = data; break;
+                    case 5: imu_dat.gyro_y = data; break;
+                    case 6: imu_dat.gyro_z = data; break;
+                }
+            }
+
+            Wire.beginTransmission(IMU_ID);
+            Wire.write(IMU_REG);
+            Wire.endTransmission(false);
+            Wire.requestFrom(IMU_ID, num_Reg * 2, true);
+        }
     }
 }
 
 void IMU_Init() {
+    Wire.setSDA(SDA_PIN);
+    Wire.setSCL(SCL_PIN);
+    Wire.begin();
+
+    Wire.beginTransmission(IMU_ID);
+    Wire.write(0);
+    Wire.endTransmission(true);
+
     xTaskCreate(IMU_Read, "threadIMU", THREAD_IMU_STACK_SIZE, NULL,
                 THREAD_IMU_PRIORITY, NULL);
 }
--- fsae-vehicle-fw/src/peripherals/imu.h
+++ fsae-vehicle-fw/src/peripherals/imu.h
@@ -3,7 +3,25 @@
 #ifndef IMU_H
 #define IMU_H
 
+#include <Wire.h>
+
+#define SDA_PIN 25
+#define SCL_PIN 24
+
+#define IMU_ID 0x68
+#define IMU_REG 0x3B
+#define num_Reg 7
+
 // TODO: Define IMU Struct
+typedef struct {
+    int16_t accel_x;
+    int16_t accel_y;
+    int16_t accel_z;
+    int16_t temp;
+    int16_t gyro_x;
+    int16_t gyro_y;
+    int16_t gyro_z;
+} IMUData
 
 void IMU_Read(void *pvParameters);
 void IMU_Init();
