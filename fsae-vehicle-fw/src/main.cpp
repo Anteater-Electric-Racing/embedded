b0fc283e614dad1427d3ecf288781ac31435e300
Natalie Perrochon <nperroch@uci.edu>
2025-04-19 15:38:51 -0700

    Change to teensy 4.1 in platformioinit

--- fsae-vehicle-fw/src/main.cpp
+++ fsae-vehicle-fw/src/main.cpp
@@ -25,8 +25,9 @@ void setup() { // runs once on bootup
 void threadMain(void *pvParameters) {
     Serial.begin(9600);
 
-    Peripherals_Init();
-    Faults_Init();
+    // Peripherals_Init();
+
+    // Faults_Init();
     Telemetry_Init();
     CAN_Init();
 
