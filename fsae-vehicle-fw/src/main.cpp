68b2a25267ecab9b5cf72fd99c8d8ccb9afcd2ed
WilsonNguyen <wilsonnguyen0928@gmail.com>
2025-04-24 22:44:55 -0700

    finished testing watchdog

--- fsae-vehicle-fw/src/main.cpp
+++ fsae-vehicle-fw/src/main.cpp
@@ -3,7 +3,6 @@
 #define THREAD_MAIN_STACK_SIZE 128
 #define THREAD_MAIN_PRIORITY 1
 
-
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
 #include <arduino_freertos.h>
@@ -12,49 +11,32 @@
 #include "peripherals/peripherals.h"
 #include "vehicle/faults.h"
 #include "vehicle/telemetry.h"
+#include "peripherals/watchdog.h"
 
 void threadMain( void *pvParameters );
 
 void setup() { // runs once on bootup
-    xTaskCreate(threadMain, "threadMain", THREAD_MAIN_STACK_SIZE, NULL,
-                THREAD_MAIN_PRIORITY, NULL);
+    xTaskCreate(threadMain, "threadMain", THREAD_MAIN_STACK_SIZE, NULL, THREAD_MAIN_PRIORITY, NULL);
     vTaskStartScheduler();
 }
 
 void threadMain(void *pvParameters) {
     Serial.begin(9600);
+    vTaskDelay(pdMS_TO_TICKS(100));
 
     Peripherals_Init();
-
     Faults_Init();
     Telemetry_Init();
+    Watchdog_Init();
+    Serial.println("Entering loop...");
 
     while (true) {
         // Main loop
+          Serial.println("Running...");
+          //Watchdog_Pet(); commented out for testing
+          vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
+
 void loop() {}
-
-// Update sensor last updates
-/*
-void updateBrakeSensor() {
-    sensorLastUpdate.brake = millis();
-}
-
-void updateSuspensionSensor() {
-    sensorLastUpdate.suspension = millis();
-}
-
-void updatePedalSensor() {
-    sensorLastUpdate.pedal = millis();
-}
-
-void updateImuSensor() {
-    sensorLastUpdate.imu = millis();
-}
-
-void updateGpsSensor() {
-    sensorLastUpdate.gps = millis();
-}
-*/
--- fsae-vehicle-fw/src/peripherals/watchdog.h
+++ fsae-vehicle-fw/src/peripherals/watchdog.h
@@ -9,16 +9,6 @@
 #define WDT_WINDOW_SEC 0.5
 #define WDT_TIMEOUT_SEC 1.0
 #define WDT_TRIGGER_SEC 0.8
-/*
-static struct { // delete this later
-    volatile unsigned long brake;
-    volatile unsigned long suspension;
-    volatile unsigned long pedal;
-    volatile unsigned long imu;
-    volatile unsigned long gps;
-} sensorLastUpdate = {0, 0, 0, 0, 0}; */
-
-bool checkSensors();
 
 void Watchdog_Init();
 
