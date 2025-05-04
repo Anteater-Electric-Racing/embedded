6a9c92e3bb06b2f8ecdde72005105d5493d24312
Alistair Keiller <alistair@keiller.net>
2025-05-04 16:41:39 -0700

    reformat and add while loop to pull off the entire queue

--- fsae-vehicle-fw/src/vehicle/can.cpp
+++ fsae-vehicle-fw/src/vehicle/can.cpp
@@ -1,17 +1,22 @@
 // Anteater Electric Racing, 2025
 
-#include "can.h"
 #include <FlexCAN_T4.h>
 #include <isotp.h>
+
 #include <arduino_freertos.h>
 #include <semphr.h>
+
+#include "can.h"
 #include "peripherals/adc.h"
 
 #define THREAD_CAN_STACK_SIZE 128
 #define THREAD_CAN_PRIORITY 1
 #define THREAD_CAN_TELEMETRY_PRIORITY 2
 #define QUEUE_TELEMETRY_ADC_LENGTH 3
-#define QUEUE_TELEMETRY_ADC_ITEM_SIZE (sizeof(uint16_t)*(SENSOR_PIN_AMT_ADC0 + SENSOR_PIN_AMT_ADC1))// TODO: figure out how big to make this
+#define QUEUE_TELEMETRY_ADC_ITEM_SIZE                                          \
+    (sizeof(uint16_t) *                                                        \
+     (SENSOR_PIN_AMT_ADC0 +                                                    \
+      SENSOR_PIN_AMT_ADC1)) // TODO: figure out how big to make this
 
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can3;
 CAN_message_t msg;
@@ -25,23 +30,23 @@ const uint32_t canid = 0x666;
 const uint32_t request = 0x777;
 CANTelemetryData telemetryDataCAN;
 isotp<RX_BANKS_16, 512> tp;
-FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2; // TODO: Figure out actual values for send and recieve
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16>
+    can2; // TODO: Figure out actual values for send and recieve
 
-void threadCAN( void *pvParameters );
-void threadTelemetryCAN( void *pvParameters );
+void threadCAN(void *pvParameters);
+void threadTelemetryCAN(void *pvParameters);
 
 void CAN_TelemetryInit() {
     xSemaphore = xSemaphoreCreateMutex();
     // fill with reasonable default values
-    telemetryDataCAN = {
-        .accumulatorVoltage = 0,
-        .accumulatorTemp = 0,
-        .tractiveSystemVoltage = 0
-    };
+    telemetryDataCAN = {.accumulatorVoltage = 0,
+                        .accumulatorTemp = 0,
+                        .tractiveSystemVoltage = 0};
     // TODO: Initialize adc reads data with more helpful default values
     uint16_t adc0Reads[SENSOR_PIN_AMT_ADC0] = {0, 0, 0, 0, 0, 0, 0, 0};
     uint16_t adc1Reads[SENSOR_PIN_AMT_ADC1] = {0, 0, 0, 0, 0, 0, 0, 0};
-    // CAN_SerializeTelemetryData(telemetryDataCAN, ); // May be problematic if we forget to call this somewhere after updating data
+    // CAN_SerializeTelemetryData(telemetryDataCAN, ); // May be problematic if
+    // we forget to call this somewhere after updating data
 }
 
 void CAN_Init() {
@@ -68,15 +73,18 @@ void CAN_Init() {
 }
 
 void CAN_Begin() {
-    // xTaskCreate(threadCAN, "threadCAN", THREAD_CAN_STACK_SIZE, NULL, THREAD_CAN_PRIORITY, NULL);
-    xTaskCreate(threadTelemetryCAN, "threadTelemetryCAN", THREAD_CAN_STACK_SIZE, NULL, THREAD_CAN_TELEMETRY_PRIORITY, NULL);
-    adcValuesQueue = xQueueCreate( QUEUE_TELEMETRY_ADC_LENGTH, QUEUE_TELEMETRY_ADC_ITEM_SIZE );
+    // xTaskCreate(threadCAN, "threadCAN", THREAD_CAN_STACK_SIZE, NULL,
+    // THREAD_CAN_PRIORITY, NULL);
+    xTaskCreate(threadTelemetryCAN, "threadTelemetryCAN", THREAD_CAN_STACK_SIZE,
+                NULL, THREAD_CAN_TELEMETRY_PRIORITY, NULL);
+    adcValuesQueue =
+        xQueueCreate(QUEUE_TELEMETRY_ADC_LENGTH, QUEUE_TELEMETRY_ADC_ITEM_SIZE);
 }
 
-void threadCAN(void *pvParameters){
-    while(true){
+void threadCAN(void *pvParameters) {
+    while (true) {
         msg.id = 0x123; // Set the CAN ID
-        msg.len = 8; // Set the length of the message
+        msg.len = 8;    // Set the length of the message
         // int res = can3.write(MB0, msg)
         int res = can3.write(msg);
         // can3.mailboxStatus();
@@ -87,47 +95,48 @@ void threadCAN(void *pvParameters){
     }
 }
 
-void threadTelemetryCAN(void *pvParameters){
-    static uint32_t sendTimer;
-    sendTimer = millis();
+void threadTelemetryCAN(void *pvParameters) {
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    const TickType_t xFrequency = 1000;
     uint8_t serializedTelemetryBuf[sizeof(CANTelemetryData)];
     CANADCValues newValues;
-    while(true){
-        if ( millis() - sendTimer > 1000 ) {
-                Serial.print("Semaphor take successful");
-                xSemaphoreTake(xSemaphore, (TickType_t) 1000);
-                if (uxQueueMessagesWaiting(adcValuesQueue) >= 1){
-                    xQueueReceive(adcValuesQueue, &newValues, portMAX_DELAY);
-                    memcpy(telemetryDataCAN.adc0Reads, newValues.adc0Reads, sizeof(newValues.adc0Reads));
-                    memcpy(telemetryDataCAN.adc1Reads, newValues.adc1Reads, sizeof(newValues.adc1Reads));
-                }
-                CAN_SerializeTelemetryData(telemetryDataCAN, serializedTelemetryBuf);
-                xSemaphoreGive(xSemaphore);
-
-                ISOTP_data config;
-                config.id = 0x666;
-                config.flags.extended = 0; /* standard frame */
-                config.separation_time = 10; /* time between back-to-back frames in millisec */
-                tp.write(config, serializedTelemetryBuf, sizeof(serializedTelemetryBuf));
-
-                sendTimer = millis();
+    while (true) {
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        xSemaphoreTake(xSemaphore, (TickType_t)10);
+        while (uxQueueMessagesWaiting(adcValuesQueue)) {
+            xQueueReceive(adcValuesQueue, &newValues, portMAX_DELAY);
+            memcpy(telemetryDataCAN.adc0Reads, newValues.adc0Reads,
+                   sizeof(newValues.adc0Reads));
+            memcpy(telemetryDataCAN.adc1Reads, newValues.adc1Reads,
+                   sizeof(newValues.adc1Reads));
+            CAN_SerializeTelemetryData(telemetryDataCAN,
+                                       serializedTelemetryBuf);
         }
+        xSemaphoreGive(xSemaphore);
+
+        ISOTP_data config;
+        config.id = 0x666;
+        config.flags.extended = 0; /* standard frame */
+        config.separation_time =
+            10; /* time between back-to-back frames in millisec */
+        tp.write(config, serializedTelemetryBuf,
+                 sizeof(serializedTelemetryBuf));
     }
 }
 
-void floatToBytes(float val, uint8_t* buf) {
-    memcpy(buf, &val, sizeof(float));
-}
+void floatToBytes(float val, uint8_t *buf) { memcpy(buf, &val, sizeof(float)); }
 
-void CAN_SerializeTelemetryData(CANTelemetryData data, uint8_t* serializedTelemetryBuf){
+void CAN_SerializeTelemetryData(CANTelemetryData data,
+                                uint8_t *serializedTelemetryBuf) {
     memcpy(serializedTelemetryBuf, &data, sizeof(data));
 }
 
-void CAN_UpdateTelemetryData(CANTelemetryData* data) {
+void CAN_UpdateTelemetryData(CANTelemetryData *data) {
     telemetryDataCAN = *data;
 }
 
-void CAN_UpdateAccumulatorData(float accumulatorVoltage, float accumulatorTemp, float tractiveSystemVoltage){
+void CAN_UpdateAccumulatorData(float accumulatorVoltage, float accumulatorTemp,
+                               float tractiveSystemVoltage) {
     // xSemaphoreTake(xSemaphore, (TickType_t) 1000);
     telemetryDataCAN.accumulatorVoltage = accumulatorVoltage;
     telemetryDataCAN.accumulatorTemp = accumulatorTemp;
@@ -135,13 +144,17 @@ void CAN_UpdateAccumulatorData(float accumulatorVoltage, float accumulatorTemp, 
     // xSemaphoreGive(xSemaphore);
 }
 
-void CAN_UpdateTelemetryADCData(volatile uint16_t* adc0reads, volatile uint16_t* adc1reads){
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE; // TODO: check if this is correct
-    CANADCValues newValues = {.adc0Reads = *adc0reads, .adc1Reads = *adc1reads} ;
-    xQueueSendToBackFromISR(adcValuesQueue, &newValues, &xHigherPriorityTaskWoken);
+void CAN_UpdateTelemetryADCData(volatile uint16_t *adc0reads,
+                                volatile uint16_t *adc1reads) {
+    BaseType_t xHigherPriorityTaskWoken =
+        pdFALSE; // TODO: check if this is correct
+    CANADCValues newValues = {.adc0Reads = *adc0reads, .adc1Reads = *adc1reads};
+    xQueueSendToBackFromISR(adcValuesQueue, &newValues,
+                            &xHigherPriorityTaskWoken);
 
     // xSemaphoreTake(xSemaphore, (TickType_t) 1000);
-    // memcpy(telemetryDataCAN.adc0Reads, (const uint16_t*)adc0reads, sizeof(telemetryDataCAN.adc0Reads));
-    // memcpy(telemetryDataCAN.adc1Reads, (const uint16_t*)adc1reads, sizeof(telemetryDataCAN.adc1Reads));
+    // memcpy(telemetryDataCAN.adc0Reads, (const uint16_t*)adc0reads,
+    // sizeof(telemetryDataCAN.adc0Reads)); memcpy(telemetryDataCAN.adc1Reads,
+    // (const uint16_t*)adc1reads, sizeof(telemetryDataCAN.adc1Reads));
     // xSemaphoreGive(xSemaphore);
 }
