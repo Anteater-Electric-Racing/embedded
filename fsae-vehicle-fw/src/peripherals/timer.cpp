e672b980b08dbef42fedb2989ed6ead4d67fea48
Natalie Perrochon <nperroch@uci.edu>
2025-05-04 17:45:46 -0700

    Scan ADC sensors instead of mocking data for raspi testing

--- fsae-vehicle-fw/src/peripherals/timer.cpp
+++ fsae-vehicle-fw/src/peripherals/timer.cpp
@@ -7,7 +7,7 @@
 #define THREAD_CAN_TELEMETRY_PRIORITY 2
 
 void Timer_StartADCScan(){
-    // readPinTimer.begin(StartADCScanCallback, 1000);
+    readPinTimer.begin(StartADCScanCallback, 1000);
     // readPinTimer.begin(updateADCDataForRaspiTesting, 900);
-    xTaskCreate(updateADCDataForRaspiTesting, "threadADCUpdate", THREAD_CAN_STACK_SIZE, NULL, THREAD_CAN_TELEMETRY_PRIORITY, NULL);
+    // xTaskCreate(updateADCDataForRaspiTesting, "threadADCUpdate", THREAD_CAN_STACK_SIZE, NULL, THREAD_CAN_TELEMETRY_PRIORITY, NULL);
 }
