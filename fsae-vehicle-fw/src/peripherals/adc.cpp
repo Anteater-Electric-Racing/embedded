e500199e34765add0abd4dc3a9fda70aeb974379
Natalie Perrochon <nperroch@uci.edu>
2025-05-04 17:43:55 -0700

    Update pin values so they are not hex but actual teensy pins

--- fsae-vehicle-fw/src/peripherals/adc.cpp
+++ fsae-vehicle-fw/src/peripherals/adc.cpp
@@ -3,11 +3,11 @@
 #include "peripherals/adc.h"
 #include "callbacks.h"
 
-uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7};
+uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {A0, A1, A2, A3, A4, A5, A6, A7};
 volatile uint16_t adc0Index = 0;
 uint16_t adc0Reads[SENSOR_PIN_AMT_ADC0];
 
-uint16_t adc1Pins[SENSOR_PIN_AMT_ADC1] = {0xA7, 0xA6, 0xA5, 0xA4, 0xA3, 0xA2, 0xA1, 0xA0};
+uint16_t adc1Pins[SENSOR_PIN_AMT_ADC1] = {A7, A6, A5, A4, A3, A2, A1, A0};
 volatile uint16_t adc1Index = 0;
 uint16_t adc1Reads[SENSOR_PIN_AMT_ADC1];
 
