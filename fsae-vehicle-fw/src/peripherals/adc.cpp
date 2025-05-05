eae0fd76adab940d802224a9f301c712b3e2b0be
Natalie Perrochon <nperroch@uci.edu>
2025-05-04 18:28:48 -0700

    Add loop to ADC thread to run infinitely

--- fsae-vehicle-fw/src/peripherals/adc.cpp
+++ fsae-vehicle-fw/src/peripherals/adc.cpp
@@ -18,11 +18,11 @@
 
 // SemaphoreHandle_t xSemaphore;
 
-uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0]; // = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
+uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {A0, A1, A2, A3, A4, A5, A6, A7}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
 volatile uint16_t adc0Index; // = 0;
 volatile uint16_t adc0Reads[SENSOR_PIN_AMT_ADC0];
 
-uint16_t adc1Pins[SENSOR_PIN_AMT_ADC1]; // = {0xA7, 0xA6, 0xA5, 0xA4, 0xA3, 0xA2, 0xA1, 0xA0}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
+uint16_t adc1Pins[SENSOR_PIN_AMT_ADC1] = {A7, A6, A5, A4, A3, A2, A1, A0}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21, 24, 25, 19, 18, 14, 15, 17};
 volatile uint16_t adc1Index; // = 0;
 volatile uint16_t adc1Reads[SENSOR_PIN_AMT_ADC1];
 
@@ -53,24 +53,29 @@ void ADC_Begin() {
 }
 
 void threadADC( void *pvParameters ){
-    for(uint16_t currentIndexADC0 = 0; currentIndexADC0 < SENSOR_PIN_AMT_ADC0; ++currentIndexADC0){
-        uint16_t currentPinADC0 = adc0Pins[currentIndexADC0];
-        uint16_t adcRead = adc->adc1->analogRead(currentPinADC0); // in callbacks.h
-        // ad->startSynchronizedSingleRead
-        // uint16_t adcRead = currentIndexADC0; // For testing TODO change
-        adc0Reads[currentIndexADC0] = adcRead;
+    TickType_t lastWakeTime = xTaskGetTickCount();
+    const TickType_t xFrequency = 1;
+
+    while(true){
+        vTaskDelayUntil(&lastWakeTime, xFrequency);
+        for(uint16_t currentIndexADC0 = 0; currentIndexADC0 < SENSOR_PIN_AMT_ADC0; ++currentIndexADC0){
+            uint16_t currentPinADC0 = adc0Pins[currentIndexADC0];
+            uint16_t adcRead = 1; //adc->adc1->analogRead(currentPinADC0); // in callbacks.h
+            // ad->startSynchronizedSingleRead
+            // uint16_t adcRead = currentIndexADC0; // For testing TODO change
+            adc0Reads[currentIndexADC0] = adcRead;
+        }
+
+        for(uint16_t currentIndexADC1 = 0; currentIndexADC1 < SENSOR_PIN_AMT_ADC1; ){
+            uint16_t currentPinADC1 = adc1Pins[currentIndexADC1];
+            uint16_t adcRead = 2; //adc->adc1->analogRead(currentPinADC1); // in callbacks.h
+            // uint16_t adcRead = currentIndexADC1; // For testing TODO change
+            adc0Reads[currentIndexADC1] = adcRead;
+            ++currentIndexADC1;
+        }
+
+        CAN_UpdateTelemetryADCData(adc0Reads, adc1Reads);
     }
-
-    for(uint16_t currentIndexADC1 = 0; currentIndexADC1 < SENSOR_PIN_AMT_ADC1; ){
-        uint16_t currentPinADC1 = adc1Pins[currentIndexADC1];
-        uint16_t adcRead = adc->adc1->analogRead(currentPinADC1); // in callbacks.h
-        // uint16_t adcRead = currentIndexADC1; // For testing TODO change
-        adc0Reads[currentIndexADC1] = adcRead;
-        ++currentIndexADC1;
-    }
-
-    CAN_UpdateTelemetryADCData(adc0Reads, adc1Reads);
-
 }
 
 # endif
