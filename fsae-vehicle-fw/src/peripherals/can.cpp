f3d14f712092a9cbc87bccf5cf19f36df184c6b1
billofright <bill.yuan27@gmail.com>
2025-05-11 14:40:01 -0700

    added bms1 bitmap

--- fsae-vehicle-fw/src/peripherals/can.cpp
+++ fsae-vehicle-fw/src/peripherals/can.cpp
@@ -13,30 +13,60 @@
 
 #define CAN_INSTANCE CAN1
 #define CAN_BAUD_RATE 500000
-#define VCU1_ID 0x101
-#define VCU1_LEN 8 // bytes
+
+#define mVCU1_ID 0x101
+#define mVCU1_LEN 8 // bytes
+
+#define mBMS1_ID 0x1A0
+#define mBMS1_LEN 8 // bytes
 
 typedef struct __attribute__((packed)) {
-    uint64_t TorqueReq : 8;
-    uint64_t MotorSpdReq : 16;
-    uint64_t ChangeGearAlarm: 1;
-    uint64_t VCUAuthStatus : 2;
-    uint64_t Reserved : 5;
-    uint64_t VehicleState: 1;
-    uint64_t Brake_Pedal_State: 2;
-    uint64_t BMS_Main_Relay_Cmd: 1;
-    uint64_t GearLevelPos_Sts : 3;
-    uint64_t GearLevelPos_Sts_F : 1;
-    uint64_t AC_Control_Cmd : 1;
-    uint64_t VCU_WorkMode : 1;
-    uint64_t VCU_MotorMode : 2;
-    uint64_t VCU_Warning_Level : 2;
-    uint64_t KeyPosition : 2;
-    uint64_t BMS_Aux_Relay_Cmd: 1;
-    uint64_t PowerReduceReq : 3;
-    uint64_t RollingCounter : 4;
-    uint64_t Checksum : 8;
+    uint64_t sMotorTorqueReq : 8; // end of byte 0
+    uint64_t sMotorSpdReq : 16; // end of byte 1 and 2
+    uint64_t sChangeGearAlarm: 1;
+    uint64_t sAuthenticationSts : 2;
+    uint64_t Reserved : 5; // end of byte 3
+    uint64_t sGearLevelPos_Sts_F : 1;
+    uint64_t sGearLevelPos_Sts : 3;
+    uint64_t sMainRelayCmd: 1;
+    uint64_t sBrakePedal_Sts: 2;
+    uint64_t sVehicleState: 1; // end of byte 4
+    uint64_t sKeyPosition : 2;
+    uint64_t sWarningLevelVCU : 2;
+    uint64_t sMotorModeReq : 2;
+    uint64_t sWorkModeReq : 1;
+    uint64_t sAC_ControlCmd : 1; // end of byte 5
+    uint64_t sPowerReduceReq : 3;
+    uint64_t sAuxRelayCmd: 1;
+    uint64_t sVCU1_RC : 4; // end of byte 6
+    uint64_t sVCU1_CS : 8; // end of byte 7
 } VCU1;
+
+typedef struct __attribute__((packed)) {
+    uint64_t sBatChargeSts_F : 1;
+    uint64_t sHVLockSts: 1;
+    uint64_t sPositiveRelay_FB : 1;
+    uint64_t sNegativeRelay_FB : 1;
+    uint64_t sBatChargeSts : 2;
+    uint64_t sWarningLevelBMS : 2; // end of byte 0
+    uint64_t sBatSocValue: 8; // end of byte 1
+    uint64_t sBatSohValue: 8; // end of byte 2
+    uint64_t sReqMode : 2;
+    uint64_t sPrecharge_Finshed_F : 1;
+    uint64_t sPrecharge_Finished : 1;
+    uint64_t sFastChargeRelay_FB : 1;
+    uint64_t sPrechargeRelay_FB : 1;
+    uint64_t sCmd_ACDC : 2; // end of byte 3
+    uint64_t sRlAdhereDetSts : 1;
+    uint64_t sChrgRlAdhereDetSts : 1;
+    uint64_t sAllowMaxDischarge : 14; // end of byte 4 and byte 5
+    uint64_t sHVLockSts_F: 1;
+    uint64_t sResInsulationLow_F : 1;
+    uint64_t sBatPackMatchAlarm : 1;
+    uint64_t sBatPackCoincidenceAlarm : 1;
+    uint64_t sBMS1_RC : 4; // end of byte 6
+    uint64_t sBMS1_CS : 8; // end of byte 7
+} BMS1;
 
 static uint8_t ComputeChecksum(uint8_t* data, uint8_t length);
 
@@ -58,13 +88,13 @@ void CAN_Init() {
 
 void CAN_SendVCU1Message(float torqueValue)
 {
-    motorMsg.id = VCU1_ID;
-    motorMsg.len = VCU1_LEN;
+    motorMsg.id = mVCU1_ID;
+    motorMsg.len = mVCU1_LEN;
 
     VCU1 vcu1 = {0};
 
     // Map throttle percentage to uint8 value
-    vcu1.TorqueReq = (uint8_t) LINEAR_MAP(torqueValue, 0.0F, 1.0F, 0.0F, 255.0F);
+    vcu1.sMotorTorqueReq = (uint8_t) LINEAR_MAP(torqueValue, 0.0F, 1.0F, 0.0F, 255.0F);
 
     memcpy(motorMsg.buf, &vcu1, sizeof(vcu1));
     motorMsg.buf[7] = ComputeChecksum(motorMsg.buf, 8);
