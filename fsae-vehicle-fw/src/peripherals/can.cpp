a71b73eb4ce67d7995daf9cd125e582267385577
billofright <bill.yuan27@gmail.com>
2025-05-11 15:22:17 -0700

    added bitmaps for mcu 1, 2, 3

--- fsae-vehicle-fw/src/peripherals/can.cpp
+++ fsae-vehicle-fw/src/peripherals/can.cpp
@@ -51,14 +51,4 @@ void CAN_SendVCU1Message(float torqueValue)
     }
 }
 
-// checksum = (byte0 + byte1 + byte2 + byte3 + byte4 + byte5 + byte6) XOR 0xFF
-static uint8_t ComputeChecksum(uint8_t* data, uint8_t length) {
-    uint8_t sum = 0;
-    for (uint8_t i = 0; i < length - 1; i++) {
-        sum += data[i];
-    }
-    return sum ^ 0xFF;
-}
-
-
 
