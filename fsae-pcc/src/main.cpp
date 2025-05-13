cd0790c693d80712a9e01d007a312c234d941c8a
adamhwu <wuadam42@gmail.com>
2025-05-13 16:42:51 -0700

    created monitorShutdownCircuitTask() to check if TS is 90% of accumulator

--- fsae-pcc/src/main.cpp
+++ fsae-pcc/src/main.cpp
@@ -12,6 +12,11 @@
 #define SHUTDOWN_CIRCUIT_PRIORITY 2
 #define SHUTDOWN_CIRCUIT_STACK_SIZE 512
 
+// monitorShutdownCircuitTask defines
+#define ACCUMULATOR_VOLTAGE_PIN 21 
+#define TS_VOLTAGE_PIN 22
+#define PCC_RATIO .9
+
 
 #include <Arduino.h>
 #include <arduino_freertos.h>
@@ -20,7 +25,31 @@
 void threadMain(void *pvParameters);
 void prechargeTask(void *pvParameters);
 void updateStatusTask(void *pvParameters);
-void monitorShutdownCircuitTask(void *pvParamters);
+void monitorShutdownCircuitTask(void *pvParamters){
+    float accumulator_voltage, ts_voltage;
+    do {
+        accumulator_voltage = getVoltage(ACCUMULATOR_VOLTAGE_PIN);
+        ts_voltage = getVoltage(TS_VOLTAGE_PIN);
+    } 
+    while(ts_voltage < accumulator_voltage * PCC_RATIO);
+    // update variable containing voltage state
+}
+
+float getVoltage(int pin){
+    float voltage, freq = getFrequency(pin);
+    // still need to convert freq to voltage here
+    return voltage;
+}
+
+float getFrequency(int pin){
+    const unsigned int TIMEOUT = 10000;
+    unsigned int tHigh = pulseIn(pin, 1, TIMEOUT);  // microseconds
+    unsigned int tLow = pulseIn(pin, 0, TIMEOUT);
+    if (tHigh == 0 || tLow == 0){
+        return 0; // timed out
+    }
+    return ( 1000000.0 / (float)(tHigh + tLow) );    // f = 1/T
+}
 
 void setup() { // runs once on bootup
     xTaskCreate(threadMain, "threadMain", THREAD_MAIN_STACK_SIZE, NULL,
