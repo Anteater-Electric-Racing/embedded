b0fbabc71efaebe1235d453b401a809fc8349db9
Tyler Nguyen <tylerkn1@uci.edu>
2025-05-22 19:52:27 -0700

    starting telemetry thread

--- fsae-vehicle-fw/src/vehicle/telemetry.cpp
+++ fsae-vehicle-fw/src/vehicle/telemetry.cpp
@@ -1,7 +1,17 @@
 // Anteater Electric Racing, 2025
 
+#define THREAD_CAN_STACK_SIZE 128
+#define THREAD_CAN_PRIORITY 1
+
 #include "telemetry.h"
 #include "utils/circular_buf.h"
+#include <arduino_freertos.h>
+
+#include "vehicle/bse.h"
+#include "vehicle/apps.h"
+#include "vehicle/faults.h"
+#include "vehicle/telemetry.h"
+#include "vehicle/motor.h"
 
 TelemetryData telemetryData;
 static CircularBuffer<TelemetryData> telemetryBuffer(10);
@@ -13,6 +23,21 @@ void Telemetry_Init() {
         .accumulatorTemp = 0,
         .tractiveSystemVoltage = 0
     };
+
+    xTaskCreate(threadTelemetry, "threadTelemetry", THREAD_CAN_STACK_SIZE, NULL, 
+        THREAD_CAN_PRIORITY, NULL);
+}
+
+void threadTelemetry(void *pvParameters) {
+    while (true) {
+        // mutex lock
+        telemetryData.APPS_Travel = APPS_GetAPPSReading();
+        telemetryData.BSEFront_PSI = BSE_GetBSEReading()->bseFront_PSI;
+        telemetryData.BSERear_PSI = BSE_GetBSEReading()->bseRear_PSI;
+        telemetryData.motorState = Motor_GetState();
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
 bool Telemetry_GetData(TelemetryData& data) {
@@ -21,4 +46,13 @@ bool Telemetry_GetData(TelemetryData& data) {
 
 void Telemetry_UpdateData(const TelemetryData& data) {
     telemetryBuffer.put(data);
+}
+
+void printTelemetryData(const TelemetryData& data) {
+    Serial.print("Accumulator Voltage: ");
+    Serial.println(data.accumulatorVoltage);
+    Serial.print("Accumulator Temperature: ");
+    Serial.println(data.accumulatorTemp);
+    Serial.print("Tractive System Voltage: ");
+    Serial.println(data.tractiveSystemVoltage);
 }
--- fsae-vehicle-fw/src/vehicle/telemetry.h
+++ fsae-vehicle-fw/src/vehicle/telemetry.h
@@ -4,10 +4,17 @@
 #define TELEMETRY_H
 
 typedef struct {
+    float APPS_Travel; // APPS travel in %
+
+    float BSEFront_PSI; // front brake pressure in PSI
+    float BSERear_PSI; // rear brake pressure in PSI
+
     float accumulatorVoltage = 0.0f;
     float accumulatorTemp = 0.0f;
 
     float tractiveSystemVoltage = 0.0f;
+
+    MotorState motorState; // motor state
 } TelemetryData;
 
 void Telemetry_Init();
