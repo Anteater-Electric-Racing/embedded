738380f7692274cd96fce0decc06486077b5ab71
Natalie Perrochon <nperroch@uci.edu>
2025-05-24 13:55:26 -0700

    Add parentheses to macros for PEMDAS
    Order of operations was not executing correctly and clearing fault bitmap when clearing a higher fault.
    This change makes it so each fault mask clears only that fault and no others.

--- fsae-vehicle-fw/src/vehicle/faults.cpp
+++ fsae-vehicle-fw/src/vehicle/faults.cpp
@@ -1,12 +1,12 @@
 // Anteater Electric Racing, 2025
 
-#define FAULT_OVER_CURRENT_MASK 0x1
-#define FAULT_UNDER_VOLTAGE_MASK 0x1 << 1
-#define FAULT_OVER_TEMP_MASK 0x1 << 2
-#define FAULT_APPS_MASK 0x1 << 3
-#define FAULT_BSE_MASK 0x1 << 4
-#define FAULT_BPPS_MASK 0x1 << 5
-#define FAULT_APPS_BRAKE_PLAUSIBILITY_MASK 0x1 << 6
+#define FAULT_OVER_CURRENT_MASK (0x1)
+#define FAULT_UNDER_VOLTAGE_MASK (0x1 << 1)
+#define FAULT_OVER_TEMP_MASK (0x1 << 2)
+#define FAULT_APPS_MASK (0x1 << 3)
+#define FAULT_BSE_MASK (0x1 << 4)
+#define FAULT_BPPS_MASK (0x1 << 5)
+#define FAULT_APPS_BRAKE_PLAUSIBILITY_MASK (0x1 << 6)
 
 #include "vehicle/faults.h"
 #include "vehicle/motor.h"
