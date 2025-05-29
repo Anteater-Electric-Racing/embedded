1c6cbe9403b8c771ec7c1544df349e3e92e2dd6b
Alistair Keiller <alistair@keiller.net>
2025-05-29 16:15:11 -0700

    update fsae-raspi to isotp

--- fsae-raspi/src/main.rs
+++ fsae-raspi/src/main.rs
@@ -1,22 +1,14 @@
 mod can;
-mod uart;
 mod mqtt;
 mod send;
 use can::read_can;
-use uart::read_uart;
 use mqtt::mqttd;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
     tokio::spawn(read_can());
-    
-    tokio::spawn(read_uart());
-    
-    std::thread::spawn(mqttd);
 
-    tokio::signal::ctrl_c().await?;
+    mqttd();
 
     Ok(())
 }
