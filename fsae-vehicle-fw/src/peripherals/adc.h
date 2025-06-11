77f8bb5a41d57cd0c8d8a5ee081aff64fe62992d
Karan <ksthakkar52@gmail.com>
2025-06-11 00:03:13 -0700

    add base thermal files (simple if loop)

--- fsae-vehicle-fw/src/peripherals/adc.h
+++ fsae-vehicle-fw/src/peripherals/adc.h
@@ -2,8 +2,8 @@
 #pragma once
 
 #include <ADC.h>
-#define SENSOR_PIN_AMT_ADC0 8
-#define SENSOR_PIN_AMT_ADC1 8
+#define SENSOR_PIN_AMT_ADC0 11
+#define SENSOR_PIN_AMT_ADC1 11
 
 extern uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0];
 extern uint16_t adc0Index;
