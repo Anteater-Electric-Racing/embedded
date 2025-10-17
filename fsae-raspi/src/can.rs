186f8bff6f835be8c2d86ce5775b08b0cedfb8df
Alistair Keiller <alistair@keiller.net>
2025-10-17 20:33:38 +0000

    start upgrade to influxdb3

--- fsae-raspi/src/can.rs
+++ fsae-raspi/src/can.rs
@@ -1,8 +1,7 @@
 use std::time::Duration;
 
 use crate::send::{send_message, Reading};
-use chrono::{DateTime, Utc};
-use influxdb::InfluxDbWriteable;
+use rand::Rng;
 use serde::Serialize;
 use tokio::time::sleep;
 use tokio_socketcan_isotp::{IsoTpSocket, StandardId};
@@ -10,18 +9,10 @@ use tokio_socketcan_isotp::{IsoTpSocket, StandardId};
 const CAN_INTERFACE: &str = "can0";
 
 macro_rules! define_enum {
-    ($name:ident, $($variant:ident = $value:expr => $text:expr),*) => {
+    ($name:ident, $($variant:ident = $value:expr),*) => {
         #[derive(Serialize)]
         enum $name {
             $($variant = $value),*
-        }
-
-        impl Into<influxdb::Type> for $name {
-            fn into(self) -> influxdb::Type {
-                influxdb::Type::Text(match self {
-                    $(Self::$variant => $text.to_string()),*
-                })
-            }
         }
 
         impl $name {
@@ -35,55 +26,59 @@ macro_rules! define_enum {
     };
 }
 
-define_enum!(MotorState,
-    MotorStateOff = 0 => "Off",
-    MotorStatePrecharging = 1 => "Precharging",
-    MotorStateIdle = 2 => "Idle",
-    MotorStateDriving = 3 => "Driving",
-    MotorStateFault = 4 => "Fault"
+define_enum!(
+    MotorState,
+    MotorStateOff = 0,
+    MotorStatePrecharging = 1,
+    MotorStateIdle = 2,
+    MotorStateDriving = 3,
+    MotorStateFault = 4
 );
 
-define_enum!(MotorRotateDirection,
-    DirectionStandby = 0 => "Standby",
-    DirectionForward = 1 => "Forward",
-    DirectionBackward = 2 => "Backward",
-    DirectionError = 3 => "Error"
+define_enum!(
+    MotorRotateDirection,
+    DirectionStandby = 0,
+    DirectionForward = 1,
+    DirectionBackward = 2,
+    DirectionError = 3
 );
 
-define_enum!(MCUMainState,
-    StateStandby = 0 => "Standby",
-    StatePrecharge = 1 => "Precharge",
-    StatePowerReady = 2 => "PowerReady",
-    StateRun = 3 => "Run",
-    StatePowerOff = 4 => "PowerOff"
+define_enum!(
+    MCUMainState,
+    StateStandby = 0,
+    StatePrecharge = 1,
+    StatePowerReady = 2,
+    StateRun = 3,
+    StatePowerOff = 4
 );
 
-define_enum!(MCUWorkMode,
-    WorkModeStandby = 0 => "Standby",
-    WorkModeTorque = 1 => "Torque",
-    WorkModeSpeed = 2 => "Speed"
+define_enum!(
+    MCUWorkMode,
+    WorkModeStandby = 0,
+    WorkModeTorque = 1,
+    WorkModeSpeed = 2
 );
 
-define_enum!(MCUWarningLevel,
-    ErrorNone = 0 => "None",
-    ErrorLow = 1 => "Low",
-    ErrorMedium = 2 => "Medium",
-    ErrorHigh = 3 => "High"
+define_enum!(
+    MCUWarningLevel,
+    ErrorNone = 0,
+    ErrorLow = 1,
+    ErrorMedium = 2,
+    ErrorHigh = 3
 );
 
-#[derive(Serialize, InfluxDbWriteable)]
+#[derive(Serialize)]
 struct TelemetryData {
-    time: DateTime<Utc>,
-    apps_travel: f32,
-    bse_front_psi: f32,
-    bse_rear_psi: f32,
-    accumulator_voltage: f32,
-    accumulator_temp_f: f32,
+    apps_travel: f64,
+    bse_front_psi: f64,
+    bse_rear_psi: f64,
+    accumulator_voltage: f64,
+    accumulator_temp_f: f64,
     motor_state: MotorState,
-    motor_speed: f32,
-    motor_torque: f32,
-    max_motor_torque: f32,
-    max_motor_brake_torque: f32,
+    motor_speed: f64,
+    motor_torque: f64,
+    max_motor_torque: f64,
+    max_motor_brake_torque: f64,
     motor_direction: MotorRotateDirection,
     mcu_main_state: MCUMainState,
     mcu_work_mode: MCUWorkMode,
@@ -107,13 +102,13 @@ struct TelemetryData {
     motor_stall_fault: bool,
     motor_open_phase_fault: bool,
     mcu_warning_level: MCUWarningLevel,
-    mcu_voltage: f32,
-    mcu_current: f32,
-    motor_phase_curr: f32,
-    debug_0: f32,
-    debug_1: f32,
-    debug_2: f32,
-    debug_3: f32,
+    mcu_voltage: f64,
+    mcu_current: f64,
+    motor_phase_curr: f64,
+    debug_0: f64,
+    debug_1: f64,
+    debug_2: f64,
+    debug_3: f64,
 }
 
 impl Reading for TelemetryData {
@@ -127,71 +122,119 @@ fn parse_bool(byte: u8) -> bool {
 }
 
 pub async fn read_can() {
+    // #[cfg(target_os = "linux")]
+    // loop {
+    //     let Ok(socket) = IsoTpSocket::open(
+    //         CAN_INTERFACE,
+    //         StandardId::new(0x666).expect("Invalid src id"),
+    //         StandardId::new(0x777).expect("Invalid src id"),
+    //     ) else {
+    //         println!("Failed to open socket");
+    //         sleep(Duration::from_secs(1)).await;
+    //         continue;
+    //     };
+
+    //     while let Ok(packet) = socket.read_packet().await {
+    //         if packet.len() != 94 {
+    //             println!("Invalid packet length: {}", packet.len());
+    //             continue;
+    //         }
+    //         send_message(TelemetryData {
+    //             apps_travel: f32::from_le_bytes(packet[0..4].try_into().unwrap()),
+    //             bse_front_psi: f32::from_le_bytes(packet[4..8].try_into().unwrap()),
+    //             bse_rear_psi: f32::from_le_bytes(packet[8..12].try_into().unwrap()),
+    //             accumulator_voltage: f32::from_le_bytes(packet[12..16].try_into().unwrap()),
+    //             accumulator_temp_f: f32::from_le_bytes(packet[16..20].try_into().unwrap()),
+    //             motor_state: MotorState::from_byte(packet[20]),
+    //             motor_speed: f32::from_le_bytes(packet[21..25].try_into().unwrap()),
+    //             motor_torque: f32::from_le_bytes(packet[25..29].try_into().unwrap()),
+    //             max_motor_torque: f32::from_le_bytes(packet[29..33].try_into().unwrap()),
+    //             max_motor_brake_torque: f32::from_le_bytes(packet[33..37].try_into().unwrap()),
+    //             motor_direction: MotorRotateDirection::from_byte(packet[37]),
+    //             mcu_main_state: MCUMainState::from_byte(packet[38]),
+    //             mcu_work_mode: MCUWorkMode::from_byte(packet[39]),
+    //             motor_temp: i32::from_le_bytes(packet[40..44].try_into().unwrap()),
+    //             mcu_temp: i32::from_le_bytes(packet[44..48].try_into().unwrap()),
+    //             dc_main_wire_over_volt_fault: parse_bool(packet[48]),
+    //             motor_phase_curr_fault: parse_bool(packet[49]),
+    //             mcu_over_hot_fault: parse_bool(packet[50]),
+    //             resolver_fault: parse_bool(packet[51]),
+    //             phase_curr_sensor_fault: parse_bool(packet[52]),
+    //             motor_over_spd_fault: parse_bool(packet[53]),
+    //             drv_motor_over_hot_fault: parse_bool(packet[54]),
+    //             dc_main_wire_over_curr_fault: parse_bool(packet[55]),
+    //             drv_motor_over_cool_fault: parse_bool(packet[56]),
+    //             mcu_motor_system_state: parse_bool(packet[57]),
+    //             mcu_temp_sensor_state: parse_bool(packet[58]),
+    //             motor_temp_sensor_state: parse_bool(packet[59]),
+    //             dc_volt_sensor_state: parse_bool(packet[60]),
+    //             dc_low_volt_warning: parse_bool(packet[61]),
+    //             mcu_12v_low_volt_warning: parse_bool(packet[62]),
+    //             motor_stall_fault: parse_bool(packet[63]),
+    //             motor_open_phase_fault: parse_bool(packet[64]),
+    //             mcu_warning_level: MCUWarningLevel::from_byte(packet[65]),
+    //             mcu_voltage: f32::from_le_bytes(packet[66..70].try_into().unwrap()),
+    //             mcu_current: f32::from_le_bytes(packet[70..74].try_into().unwrap()),
+    //             motor_phase_curr: f32::from_le_bytes(packet[74..78].try_into().unwrap()),
+    //             debug_0: f32::from_le_bytes(packet[78..82].try_into().unwrap()),
+    //             debug_1: f32::from_le_bytes(packet[82..86].try_into().unwrap()),
+    //             debug_2: f32::from_le_bytes(packet[86..90].try_into().unwrap()),
+    //             debug_3: f32::from_le_bytes(packet[90..94].try_into().unwrap()),
+    //         })
+    //         .await;
+    //     }
+    // }
+    // #[cfg(not(target_os = "linux"))]
     loop {
-        let Ok(socket) = IsoTpSocket::open(
-            CAN_INTERFACE,
-            StandardId::new(0x666).expect("Invalid src id"),
-            StandardId::new(0x777).expect("Invalid src id"),
-        ) else {
-            println!("Failed to open socket");
-            sleep(Duration::from_secs(1)).await;
-            continue;
+        let telemetry = {
+            let mut rng = rand::rng();
+
+            TelemetryData {
+                apps_travel: rng.random_range(0.0..100.0),
+                bse_front_psi: rng.random_range(0.0..50.0),
+                bse_rear_psi: rng.random_range(0.0..50.0),
+                accumulator_voltage: rng.random_range(0.0..600.0),
+                accumulator_temp_f: rng.random_range(32.0..212.0),
+                motor_state: MotorState::from_byte(rng.random_range(0..5)),
+                motor_speed: rng.random_range(0.0..10000.0),
+                motor_torque: rng.random_range(-100.0..100.0),
+                max_motor_torque: rng.random_range(0.0..150.0),
+                max_motor_brake_torque: rng.random_range(0.0..150.0),
+                motor_direction: MotorRotateDirection::from_byte(rng.random_range(0..4)),
+                mcu_main_state: MCUMainState::from_byte(rng.random_range(0..5)),
+                mcu_work_mode: MCUWorkMode::from_byte(rng.random_range(0..3)),
+                motor_temp: rng.random_range(-40..150),
+                mcu_temp: rng.random_range(-40..150),
+                dc_main_wire_over_volt_fault: rng.random_bool(0.1),
+                motor_phase_curr_fault: rng.random_bool(0.1),
+                mcu_over_hot_fault: rng.random_bool(0.1),
+                resolver_fault: rng.random_bool(0.1),
+                phase_curr_sensor_fault: rng.random_bool(0.1),
+                motor_over_spd_fault: rng.random_bool(0.1),
+                drv_motor_over_hot_fault: rng.random_bool(0.1),
+                dc_main_wire_over_curr_fault: rng.random_bool(0.1),
+                drv_motor_over_cool_fault: rng.random_bool(0.1),
+                mcu_motor_system_state: rng.random_bool(0.5),
+                mcu_temp_sensor_state: rng.random_bool(0.5),
+                motor_temp_sensor_state: rng.random_bool(0.5),
+                dc_volt_sensor_state: rng.random_bool(0.5),
+                dc_low_volt_warning: rng.random_bool(0.1),
+                mcu_12v_low_volt_warning: rng.random_bool(0.1),
+                motor_stall_fault: rng.random_bool(0.1),
+                motor_open_phase_fault: rng.random_bool(0.1),
+                mcu_warning_level: MCUWarningLevel::from_byte(rng.random_range(0..4)),
+                mcu_voltage: rng.random_range(0.0..600.0),
+                mcu_current: rng.random_range(-200.0..200.0),
+                motor_phase_curr: rng.random_range(-200.0..200.0),
+                debug_0: rng.random_range(-1000.0..1000.0),
+                debug_1: rng.random_range(-1000.0..1000.0),
+                debug_2: rng.random_range(-1000.0..1000.0),
+                debug_3: rng.random_range(-1000.0..1000.0),
+            }
         };
 
-        while let Ok(packet) = socket.read_packet().await {
-            if packet.len() != 94 {
-                println!("Invalid packet length: {}", packet.len());
-                continue;
-            }
-            println!("[{}] Received packet: ", Utc::now());
-            for byte in &packet {
-                print!("{:02x} ", byte);
-            }
-            println!();
-            send_message(TelemetryData {
-                time: Utc::now(),
-                apps_travel: f32::from_le_bytes(packet[0..4].try_into().unwrap()),
-                bse_front_psi: f32::from_le_bytes(packet[4..8].try_into().unwrap()),
-                bse_rear_psi: f32::from_le_bytes(packet[8..12].try_into().unwrap()),
-                accumulator_voltage: f32::from_le_bytes(packet[12..16].try_into().unwrap()),
-                accumulator_temp_f: f32::from_le_bytes(packet[16..20].try_into().unwrap()),
-                motor_state: MotorState::from_byte(packet[20]),
-                motor_speed: f32::from_le_bytes(packet[21..25].try_into().unwrap()),
-                motor_torque: f32::from_le_bytes(packet[25..29].try_into().unwrap()),
-                max_motor_torque: f32::from_le_bytes(packet[29..33].try_into().unwrap()),
-                max_motor_brake_torque: f32::from_le_bytes(packet[33..37].try_into().unwrap()),
-                motor_direction: MotorRotateDirection::from_byte(packet[37]),
-                mcu_main_state: MCUMainState::from_byte(packet[38]),
-                mcu_work_mode: MCUWorkMode::from_byte(packet[39]),
-                motor_temp: i32::from_le_bytes(packet[40..44].try_into().unwrap()),
-                mcu_temp: i32::from_le_bytes(packet[44..48].try_into().unwrap()),
-                dc_main_wire_over_volt_fault: parse_bool(packet[48]),
-                motor_phase_curr_fault: parse_bool(packet[49]),
-                mcu_over_hot_fault: parse_bool(packet[50]),
-                resolver_fault: parse_bool(packet[51]),
-                phase_curr_sensor_fault: parse_bool(packet[52]),
-                motor_over_spd_fault: parse_bool(packet[53]),
-                drv_motor_over_hot_fault: parse_bool(packet[54]),
-                dc_main_wire_over_curr_fault: parse_bool(packet[55]),
-                drv_motor_over_cool_fault: parse_bool(packet[56]),
-                mcu_motor_system_state: parse_bool(packet[57]),
-                mcu_temp_sensor_state: parse_bool(packet[58]),
-                motor_temp_sensor_state: parse_bool(packet[59]),
-                dc_volt_sensor_state: parse_bool(packet[60]),
-                dc_low_volt_warning: parse_bool(packet[61]),
-                mcu_12v_low_volt_warning: parse_bool(packet[62]),
-                motor_stall_fault: parse_bool(packet[63]),
-                motor_open_phase_fault: parse_bool(packet[64]),
-                mcu_warning_level: MCUWarningLevel::from_byte(packet[65]),
-                mcu_voltage: f32::from_le_bytes(packet[66..70].try_into().unwrap()),
-                mcu_current: f32::from_le_bytes(packet[70..74].try_into().unwrap()),
-                motor_phase_curr: f32::from_le_bytes(packet[74..78].try_into().unwrap()),
-                debug_0: f32::from_le_bytes(packet[78..82].try_into().unwrap()),
-                debug_1: f32::from_le_bytes(packet[82..86].try_into().unwrap()),
-                debug_2: f32::from_le_bytes(packet[86..90].try_into().unwrap()),
-                debug_3: f32::from_le_bytes(packet[90..94].try_into().unwrap()),
-            })
-            .await;
-        }
+        send_message(telemetry).await;
+
+        sleep(Duration::from_millis(100)).await;
     }
 }
--- fsae-raspi/src/send.rs
+++ fsae-raspi/src/send.rs
@@ -1,22 +1,27 @@
-use influxdb::{Client, InfluxDbWriteable};
+use reqwest::Client;
 use rumqttc::{AsyncClient, MqttOptions, QoS};
 use serde::Serialize;
 use std::sync::LazyLock;
 use tokio::time::Duration;
 
-pub const INFLUXDB_URL: &str = "http://127.0.0.1:8086";
+use crate::influxdb::to_line_protocol;
+
+pub const INFLUXDB_URL: &str = "http://0.0.0.0:8181";
 pub const INFLUXDB_DATABASE: &str = "fsae";
 
 pub const MQTT_ID: &str = "fsae";
 pub const MQTT_HOST: &str = "127.0.0.1";
 pub const MQTT_PORT: u16 = 1883;
 
-pub trait Reading: InfluxDbWriteable + Serialize {
+pub trait Reading: Serialize {
     fn topic() -> &'static str;
 }
 
-static INFLUX_CLIENT: LazyLock<Client> =
-    LazyLock::new(|| Client::new(INFLUXDB_URL, INFLUXDB_DATABASE));
+static INFLUX_CLIENT: LazyLock<Client> = LazyLock::new(|| {
+    reqwest::Client::builder()
+        .build()
+        .expect("Failed to build InfluxDB client")
+});
 
 static MQTT_CLIENT: LazyLock<AsyncClient> = LazyLock::new(|| {
     let mut mqttoptions = MqttOptions::new(MQTT_ID, MQTT_HOST, MQTT_PORT);
@@ -35,7 +40,7 @@ static MQTT_CLIENT: LazyLock<AsyncClient> = LazyLock::new(|| {
     mqtt_client
 });
 
-pub async fn send_message<T: Reading + Send + 'static>(message: T) {
+pub async fn send_message<T: Reading>(message: T) {
     let json = match serde_json::to_string(&message) {
         Ok(j) => j,
         Err(e) => {
@@ -44,8 +49,6 @@ pub async fn send_message<T: Reading + Send + 'static>(message: T) {
         }
     };
 
-    println!("Sending message: {}", json);
-
     if let Err(e) = MQTT_CLIENT
         .publish(T::topic(), QoS::AtLeastOnce, false, json)
         .await
@@ -53,7 +56,18 @@ pub async fn send_message<T: Reading + Send + 'static>(message: T) {
         eprintln!("Failed to publish to MQTT: {}", e);
     }
 
-    if let Err(e) = INFLUX_CLIENT.query(message.into_query(T::topic())).await {
+    let line_protocol = to_line_protocol(&message);
+    let url = format!(
+        "{}/api/v3/write_lp?db={}&precision=nanosecond&accept_partial=true&no_sync=false",
+        INFLUXDB_URL, INFLUXDB_DATABASE
+    );
+    if let Err(e) = INFLUX_CLIENT
+        .post(&url)
+        .header("Authorization", "Bearer apiv3_TQdSxXbtRc8qbzb4ejQOa-ir9-deb4fSVe5Lc-RgvQZqPKikusEJtZpQmEJakPtxZvst8wW4B20KB8iSGLC-Tg")
+        .body(line_protocol)
+        .send()
+        .await
+    {
         eprintln!("Failed to write to InfluxDB: {}", e);
     }
 }
