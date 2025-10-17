27c6cdbe643d9c745b55f528399608c2d3c5e2e0
Alistair Keiller <alistair@keiller.net>
2025-10-17 20:47:35 +0000

    go back to using isotp

--- fsae-raspi/src/can.rs
+++ fsae-raspi/src/can.rs
@@ -69,16 +69,16 @@ define_enum!(
 
 #[derive(Serialize)]
 struct TelemetryData {
-    apps_travel: f64,
-    bse_front_psi: f64,
-    bse_rear_psi: f64,
-    accumulator_voltage: f64,
-    accumulator_temp_f: f64,
+    apps_travel: f32,
+    bse_front_psi: f32,
+    bse_rear_psi: f32,
+    accumulator_voltage: f32,
+    accumulator_temp_f: f32,
     motor_state: MotorState,
-    motor_speed: f64,
-    motor_torque: f64,
-    max_motor_torque: f64,
-    max_motor_brake_torque: f64,
+    motor_speed: f32,
+    motor_torque: f32,
+    max_motor_torque: f32,
+    max_motor_brake_torque: f32,
     motor_direction: MotorRotateDirection,
     mcu_main_state: MCUMainState,
     mcu_work_mode: MCUWorkMode,
@@ -102,13 +102,13 @@ struct TelemetryData {
     motor_stall_fault: bool,
     motor_open_phase_fault: bool,
     mcu_warning_level: MCUWarningLevel,
-    mcu_voltage: f64,
-    mcu_current: f64,
-    motor_phase_curr: f64,
-    debug_0: f64,
-    debug_1: f64,
-    debug_2: f64,
-    debug_3: f64,
+    mcu_voltage: f32,
+    mcu_current: f32,
+    motor_phase_curr: f32,
+    debug_0: f32,
+    debug_1: f32,
+    debug_2: f32,
+    debug_3: f32,
 }
 
 impl Reading for TelemetryData {
@@ -122,119 +122,117 @@ fn parse_bool(byte: u8) -> bool {
 }
 
 pub async fn read_can() {
-    // #[cfg(target_os = "linux")]
-    // loop {
-    //     let Ok(socket) = IsoTpSocket::open(
-    //         CAN_INTERFACE,
-    //         StandardId::new(0x666).expect("Invalid src id"),
-    //         StandardId::new(0x777).expect("Invalid src id"),
-    //     ) else {
-    //         println!("Failed to open socket");
-    //         sleep(Duration::from_secs(1)).await;
-    //         continue;
-    //     };
-
-    //     while let Ok(packet) = socket.read_packet().await {
-    //         if packet.len() != 94 {
-    //             println!("Invalid packet length: {}", packet.len());
-    //             continue;
-    //         }
-    //         send_message(TelemetryData {
-    //             apps_travel: f32::from_le_bytes(packet[0..4].try_into().unwrap()),
-    //             bse_front_psi: f32::from_le_bytes(packet[4..8].try_into().unwrap()),
-    //             bse_rear_psi: f32::from_le_bytes(packet[8..12].try_into().unwrap()),
-    //             accumulator_voltage: f32::from_le_bytes(packet[12..16].try_into().unwrap()),
-    //             accumulator_temp_f: f32::from_le_bytes(packet[16..20].try_into().unwrap()),
-    //             motor_state: MotorState::from_byte(packet[20]),
-    //             motor_speed: f32::from_le_bytes(packet[21..25].try_into().unwrap()),
-    //             motor_torque: f32::from_le_bytes(packet[25..29].try_into().unwrap()),
-    //             max_motor_torque: f32::from_le_bytes(packet[29..33].try_into().unwrap()),
-    //             max_motor_brake_torque: f32::from_le_bytes(packet[33..37].try_into().unwrap()),
-    //             motor_direction: MotorRotateDirection::from_byte(packet[37]),
-    //             mcu_main_state: MCUMainState::from_byte(packet[38]),
-    //             mcu_work_mode: MCUWorkMode::from_byte(packet[39]),
-    //             motor_temp: i32::from_le_bytes(packet[40..44].try_into().unwrap()),
-    //             mcu_temp: i32::from_le_bytes(packet[44..48].try_into().unwrap()),
-    //             dc_main_wire_over_volt_fault: parse_bool(packet[48]),
-    //             motor_phase_curr_fault: parse_bool(packet[49]),
-    //             mcu_over_hot_fault: parse_bool(packet[50]),
-    //             resolver_fault: parse_bool(packet[51]),
-    //             phase_curr_sensor_fault: parse_bool(packet[52]),
-    //             motor_over_spd_fault: parse_bool(packet[53]),
-    //             drv_motor_over_hot_fault: parse_bool(packet[54]),
-    //             dc_main_wire_over_curr_fault: parse_bool(packet[55]),
-    //             drv_motor_over_cool_fault: parse_bool(packet[56]),
-    //             mcu_motor_system_state: parse_bool(packet[57]),
-    //             mcu_temp_sensor_state: parse_bool(packet[58]),
-    //             motor_temp_sensor_state: parse_bool(packet[59]),
-    //             dc_volt_sensor_state: parse_bool(packet[60]),
-    //             dc_low_volt_warning: parse_bool(packet[61]),
-    //             mcu_12v_low_volt_warning: parse_bool(packet[62]),
-    //             motor_stall_fault: parse_bool(packet[63]),
-    //             motor_open_phase_fault: parse_bool(packet[64]),
-    //             mcu_warning_level: MCUWarningLevel::from_byte(packet[65]),
-    //             mcu_voltage: f32::from_le_bytes(packet[66..70].try_into().unwrap()),
-    //             mcu_current: f32::from_le_bytes(packet[70..74].try_into().unwrap()),
-    //             motor_phase_curr: f32::from_le_bytes(packet[74..78].try_into().unwrap()),
-    //             debug_0: f32::from_le_bytes(packet[78..82].try_into().unwrap()),
-    //             debug_1: f32::from_le_bytes(packet[82..86].try_into().unwrap()),
-    //             debug_2: f32::from_le_bytes(packet[86..90].try_into().unwrap()),
-    //             debug_3: f32::from_le_bytes(packet[90..94].try_into().unwrap()),
-    //         })
-    //         .await;
-    //     }
-    // }
-    // #[cfg(not(target_os = "linux"))]
     loop {
-        let telemetry = {
-            let mut rng = rand::rng();
-
-            TelemetryData {
-                apps_travel: rng.random_range(0.0..100.0),
-                bse_front_psi: rng.random_range(0.0..50.0),
-                bse_rear_psi: rng.random_range(0.0..50.0),
-                accumulator_voltage: rng.random_range(0.0..600.0),
-                accumulator_temp_f: rng.random_range(32.0..212.0),
-                motor_state: MotorState::from_byte(rng.random_range(0..5)),
-                motor_speed: rng.random_range(0.0..10000.0),
-                motor_torque: rng.random_range(-100.0..100.0),
-                max_motor_torque: rng.random_range(0.0..150.0),
-                max_motor_brake_torque: rng.random_range(0.0..150.0),
-                motor_direction: MotorRotateDirection::from_byte(rng.random_range(0..4)),
-                mcu_main_state: MCUMainState::from_byte(rng.random_range(0..5)),
-                mcu_work_mode: MCUWorkMode::from_byte(rng.random_range(0..3)),
-                motor_temp: rng.random_range(-40..150),
-                mcu_temp: rng.random_range(-40..150),
-                dc_main_wire_over_volt_fault: rng.random_bool(0.1),
-                motor_phase_curr_fault: rng.random_bool(0.1),
-                mcu_over_hot_fault: rng.random_bool(0.1),
-                resolver_fault: rng.random_bool(0.1),
-                phase_curr_sensor_fault: rng.random_bool(0.1),
-                motor_over_spd_fault: rng.random_bool(0.1),
-                drv_motor_over_hot_fault: rng.random_bool(0.1),
-                dc_main_wire_over_curr_fault: rng.random_bool(0.1),
-                drv_motor_over_cool_fault: rng.random_bool(0.1),
-                mcu_motor_system_state: rng.random_bool(0.5),
-                mcu_temp_sensor_state: rng.random_bool(0.5),
-                motor_temp_sensor_state: rng.random_bool(0.5),
-                dc_volt_sensor_state: rng.random_bool(0.5),
-                dc_low_volt_warning: rng.random_bool(0.1),
-                mcu_12v_low_volt_warning: rng.random_bool(0.1),
-                motor_stall_fault: rng.random_bool(0.1),
-                motor_open_phase_fault: rng.random_bool(0.1),
-                mcu_warning_level: MCUWarningLevel::from_byte(rng.random_range(0..4)),
-                mcu_voltage: rng.random_range(0.0..600.0),
-                mcu_current: rng.random_range(-200.0..200.0),
-                motor_phase_curr: rng.random_range(-200.0..200.0),
-                debug_0: rng.random_range(-1000.0..1000.0),
-                debug_1: rng.random_range(-1000.0..1000.0),
-                debug_2: rng.random_range(-1000.0..1000.0),
-                debug_3: rng.random_range(-1000.0..1000.0),
-            }
+        let Ok(socket) = IsoTpSocket::open(
+            CAN_INTERFACE,
+            StandardId::new(0x666).expect("Invalid src id"),
+            StandardId::new(0x777).expect("Invalid src id"),
+        ) else {
+            println!("Failed to open socket");
+            sleep(Duration::from_secs(1)).await;
+            continue;
         };
 
-        send_message(telemetry).await;
-
-        sleep(Duration::from_millis(100)).await;
+        while let Ok(packet) = socket.read_packet().await {
+            if packet.len() != 94 {
+                println!("Invalid packet length: {}", packet.len());
+                continue;
+            }
+            send_message(TelemetryData {
+                apps_travel: f32::from_le_bytes(packet[0..4].try_into().unwrap()),
+                bse_front_psi: f32::from_le_bytes(packet[4..8].try_into().unwrap()),
+                bse_rear_psi: f32::from_le_bytes(packet[8..12].try_into().unwrap()),
+                accumulator_voltage: f32::from_le_bytes(packet[12..16].try_into().unwrap()),
+                accumulator_temp_f: f32::from_le_bytes(packet[16..20].try_into().unwrap()),
+                motor_state: MotorState::from_byte(packet[20]),
+                motor_speed: f32::from_le_bytes(packet[21..25].try_into().unwrap()),
+                motor_torque: f32::from_le_bytes(packet[25..29].try_into().unwrap()),
+                max_motor_torque: f32::from_le_bytes(packet[29..33].try_into().unwrap()),
+                max_motor_brake_torque: f32::from_le_bytes(packet[33..37].try_into().unwrap()),
+                motor_direction: MotorRotateDirection::from_byte(packet[37]),
+                mcu_main_state: MCUMainState::from_byte(packet[38]),
+                mcu_work_mode: MCUWorkMode::from_byte(packet[39]),
+                motor_temp: i32::from_le_bytes(packet[40..44].try_into().unwrap()),
+                mcu_temp: i32::from_le_bytes(packet[44..48].try_into().unwrap()),
+                dc_main_wire_over_volt_fault: parse_bool(packet[48]),
+                motor_phase_curr_fault: parse_bool(packet[49]),
+                mcu_over_hot_fault: parse_bool(packet[50]),
+                resolver_fault: parse_bool(packet[51]),
+                phase_curr_sensor_fault: parse_bool(packet[52]),
+                motor_over_spd_fault: parse_bool(packet[53]),
+                drv_motor_over_hot_fault: parse_bool(packet[54]),
+                dc_main_wire_over_curr_fault: parse_bool(packet[55]),
+                drv_motor_over_cool_fault: parse_bool(packet[56]),
+                mcu_motor_system_state: parse_bool(packet[57]),
+                mcu_temp_sensor_state: parse_bool(packet[58]),
+                motor_temp_sensor_state: parse_bool(packet[59]),
+                dc_volt_sensor_state: parse_bool(packet[60]),
+                dc_low_volt_warning: parse_bool(packet[61]),
+                mcu_12v_low_volt_warning: parse_bool(packet[62]),
+                motor_stall_fault: parse_bool(packet[63]),
+                motor_open_phase_fault: parse_bool(packet[64]),
+                mcu_warning_level: MCUWarningLevel::from_byte(packet[65]),
+                mcu_voltage: f32::from_le_bytes(packet[66..70].try_into().unwrap()),
+                mcu_current: f32::from_le_bytes(packet[70..74].try_into().unwrap()),
+                motor_phase_curr: f32::from_le_bytes(packet[74..78].try_into().unwrap()),
+                debug_0: f32::from_le_bytes(packet[78..82].try_into().unwrap()),
+                debug_1: f32::from_le_bytes(packet[82..86].try_into().unwrap()),
+                debug_2: f32::from_le_bytes(packet[86..90].try_into().unwrap()),
+                debug_3: f32::from_le_bytes(packet[90..94].try_into().unwrap()),
+            })
+            .await;
+        }
     }
+    // loop {
+    //     let telemetry = {
+    //         let mut rng = rand::rng();
+
+    //         TelemetryData {
+    //             apps_travel: rng.random_range(0.0..100.0),
+    //             bse_front_psi: rng.random_range(0.0..50.0),
+    //             bse_rear_psi: rng.random_range(0.0..50.0),
+    //             accumulator_voltage: rng.random_range(0.0..600.0),
+    //             accumulator_temp_f: rng.random_range(32.0..212.0),
+    //             motor_state: MotorState::from_byte(rng.random_range(0..5)),
+    //             motor_speed: rng.random_range(0.0..10000.0),
+    //             motor_torque: rng.random_range(-100.0..100.0),
+    //             max_motor_torque: rng.random_range(0.0..150.0),
+    //             max_motor_brake_torque: rng.random_range(0.0..150.0),
+    //             motor_direction: MotorRotateDirection::from_byte(rng.random_range(0..4)),
+    //             mcu_main_state: MCUMainState::from_byte(rng.random_range(0..5)),
+    //             mcu_work_mode: MCUWorkMode::from_byte(rng.random_range(0..3)),
+    //             motor_temp: rng.random_range(-40..150),
+    //             mcu_temp: rng.random_range(-40..150),
+    //             dc_main_wire_over_volt_fault: rng.random_bool(0.1),
+    //             motor_phase_curr_fault: rng.random_bool(0.1),
+    //             mcu_over_hot_fault: rng.random_bool(0.1),
+    //             resolver_fault: rng.random_bool(0.1),
+    //             phase_curr_sensor_fault: rng.random_bool(0.1),
+    //             motor_over_spd_fault: rng.random_bool(0.1),
+    //             drv_motor_over_hot_fault: rng.random_bool(0.1),
+    //             dc_main_wire_over_curr_fault: rng.random_bool(0.1),
+    //             drv_motor_over_cool_fault: rng.random_bool(0.1),
+    //             mcu_motor_system_state: rng.random_bool(0.5),
+    //             mcu_temp_sensor_state: rng.random_bool(0.5),
+    //             motor_temp_sensor_state: rng.random_bool(0.5),
+    //             dc_volt_sensor_state: rng.random_bool(0.5),
+    //             dc_low_volt_warning: rng.random_bool(0.1),
+    //             mcu_12v_low_volt_warning: rng.random_bool(0.1),
+    //             motor_stall_fault: rng.random_bool(0.1),
+    //             motor_open_phase_fault: rng.random_bool(0.1),
+    //             mcu_warning_level: MCUWarningLevel::from_byte(rng.random_range(0..4)),
+    //             mcu_voltage: rng.random_range(0.0..600.0),
+    //             mcu_current: rng.random_range(-200.0..200.0),
+    //             motor_phase_curr: rng.random_range(-200.0..200.0),
+    //             debug_0: rng.random_range(-1000.0..1000.0),
+    //             debug_1: rng.random_range(-1000.0..1000.0),
+    //             debug_2: rng.random_range(-1000.0..1000.0),
+    //             debug_3: rng.random_range(-1000.0..1000.0),
+    //         }
+    //     };
+
+    //     send_message(telemetry).await;
+
+    //     sleep(Duration::from_millis(100)).await;
+    // }
 }
