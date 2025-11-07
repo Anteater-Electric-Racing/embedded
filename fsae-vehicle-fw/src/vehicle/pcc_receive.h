e8634106fdd03ba1358a352ed338d6d0ee98d968
PranavKocharlakota <pranav.kocharlakota@gmail.com>
2025-11-07 11:39:47 -0800

    add pcc_receive.cpp

---
+++ fsae-vehicle-fw/src/vehicle/pcc_receive.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct __attribute__((packed)) {
+    //uint32_t timestamp;
+    uint8_t state;
+    uint8_t errorCode;
+    uint16_t accumulatorVoltage;
+    uint16_t tsVoltage;
+    uint16_t prechargeProgress;
+} PCC;
+
+static PCC pccData;
+
+void processPCCMessage(uint64_t);
