00d8a008f1ac6c0d85ba2cd7fde2af85ee85d5a0
Anoop Koganti <anoopkoganti05@gmail.com>
2025-12-26 16:49:10 -0800

    format code

--- fsae-vehicle-fw/src/peripherals/adc.cpp
+++ fsae-vehicle-fw/src/peripherals/adc.cpp
@@ -41,9 +41,9 @@ enum SensorIndexesADC1 { // TODO: Update with real values
 };
 
 uint16_t adc0Pins[SENSOR_PIN_AMT_ADC0] = {
-    A0, A1, A2,  A3,  A4, A5,
+    A0, A1, A2, A3,  A4, A5,
     A6, A7, A9, A16, A17}; // A4, A4, 18, 17, 17, 17, 17}; // real values: {21,
-                            // 24, 25, 19, 18, 14, 15, 17};
+                           // 24, 25, 19, 18, 14, 15, 17};
 uint16_t adc0Reads[SENSOR_PIN_AMT_ADC0];
 
 uint16_t adc1Pins[SENSOR_PIN_AMT_ADC1] = {
