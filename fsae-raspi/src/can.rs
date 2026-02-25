5296c88a9898ea6215617b73bfe256524da71639
Alistair Keiller <alistair@keiller.net>
2026-02-24 20:13:25 -0800

    add debug print

--- fsae-raspi/src/can.rs
+++ fsae-raspi/src/can.rs
@@ -151,6 +151,7 @@ pub fn parse_telemetry(packet: &[u8]) -> Result<TelemetryData, String> {
             packet.len()
         ));
     }
+    print!("Received packet: {:?}", packet);
     let fault: u32 = u32::from_le_bytes(packet[42..46].try_into().unwrap());
 
     Ok(TelemetryData {
